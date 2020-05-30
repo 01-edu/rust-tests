c150356ed239311dcb0321be47fe2d59c70c84a2
Augusto <aug.ornelas@gmail.com>
2020-05-30 16:37:14 +0100

    Add exercises to exemplify tuples and returning from a loop

---
+++ looping/src/main.rs
@@ -0,0 +1,41 @@
+// Write a program that prints a riddle, receives input from the user
+// and checks that the answer is correct
+
+// The program must allow indefinite number of trials and only quit after the
+// correct answer is given
+
+// Every time the user introduces an incorrect answer the program must
+// print the riddle
+// Before quitting the program must print the number of trials
+
+// Riddle: I am the beginning of the end, and the end of time and
+// space. I am essential to creation, and I surround every place. What
+// am I?
+
+// Answer: The letter e
+
+use std::io;
+
+fn main() {
+	let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+	let answer = "The letter e";
+	let mut counter = 0;
+
+	let trails = loop {
+		println!("{}", riddle);
+
+		let mut input = String::new();
+
+		io::stdin()
+			.read_line(&mut input)
+			.expect("Failed to read line");
+
+		counter += 1;
+
+		if answer.to_owned() + "\n" == input {
+			break counter;
+		}
+	};
+
+	println!("It took you {} trials to get the right answer", trails);
+}
