5e4b5b370c84a5690707ca9a9950a7d9ca697076
lee <lee-dasilva@hotmail.com>
2020-10-07 10:56:21 +0100

    last correction

--- options/src/lib.rs
+++ options/src/lib.rs
@@ -46,7 +46,7 @@ struct Message {
 
 #[allow(dead_code)]
 impl Message {
-  fn new_ms(ms: String, u: String, t: String) -> Message {
+  fn new(ms: String, u: String, t: String) -> Message {
     Message {
       content: ms,
       user: u,
@@ -116,9 +116,9 @@ mod tests {
   #[test]
   fn test_error_ms() {
     let v = vec![
-      Message::new_ms("".to_string(), "toby".to_string(), format_date()),
-      Message::new_ms("stupid".to_string(), "jack".to_string(), format_date()),
-      Message::new_ms(
+      Message::new("".to_string(), "toby".to_string(), format_date()),
+      Message::new("stupid".to_string(), "jack".to_string(), format_date()),
+      Message::new(
         "you are stupid".to_string(),
         "jacob".to_string(),
         format_date(),
@@ -133,18 +133,18 @@ mod tests {
   #[test]
   fn test_ok_ms() {
     let v = vec![
-      Message::new_ms(
+      Message::new(
         "get out of the car".to_string(),
         "police".to_string(),
         format_date(),
       ),
-      Message::new_ms("no!".to_string(), "thief".to_string(), format_date()),
-      Message::new_ms(
+      Message::new("no!".to_string(), "thief".to_string(), format_date()),
+      Message::new(
         "get the werewolf".to_string(),
         "police".to_string(),
         format_date(),
       ),
-      Message::new_ms(
+      Message::new(
         "wait the wha...".to_string(),
         "thief".to_string(),
         format_date(),
