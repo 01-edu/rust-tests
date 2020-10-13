f2188df1eb8a94710eb230d60445e42f10a3e3b5
Augusto <aug.ornelas@gmail.com>
2020-10-13 20:30:35 +0100

    Add extra test to blood_types exercise

--- blood_types/src/main.rs
+++ blood_types/src/main.rs
@@ -351,4 +351,17 @@ mod tests {
 		expected.sort();
 		assert_eq!(recipients, expected);
 	}
+
+	#[test]
+	fn test_output() {
+		let blood_type: BloodType = "O+".parse().unwrap();
+		println!("recipients of O+ {:?}", blood_type.recipients());
+		println!("donors of O+ {:?}", blood_type.donors());
+		let another_blood_type: BloodType = "A-".parse().unwrap();
+		println!(
+			"donors of O+ can receive from {:?} {:?}",
+			&another_blood_type,
+			blood_type.can_receive_from(&another_blood_type)
+		);
+	}
 }
