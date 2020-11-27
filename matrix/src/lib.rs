d01f5d437c427bf3472d0b5bfe1ad0d120c2e6e9
Augusto <aug.ornelas@gmail.com>
2020-11-27 17:48:22 +0000

    Add exercises `matrix_addition`

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -7,19 +7,7 @@
 
 // We will consider a matrix as a rectangular arrangements of scalars
 // Therefore you will have to create the definition of a Scalar by
-// defining a trait Scalar.
-
-// A scalar type must implement the operations
-// Addition, Subtraction, Multiplication, Modulo, Division (you might
-// also have to use more restrictions). For this use a trait
-// inheritance (supertraits)
-
-// Another condition for a number to be a scalar is to have a zero
-// (neutral element in the addition) and a one (neutral element in the
-// multiplication). Therefore the Scalar trait will require 2
-// functions zero() and one()
-
-// After finishing implement the Scalar trait for u32
+// defining a trait Scalar in another file: `scalar.rs.`
 
 // Second exercise
 
@@ -40,23 +28,9 @@
 
 // Resources: https://doc.rust-lang.org/book/ch19-03-advanced-traits.html
 
-use std::ops::{Add, Div, Mul, Rem, Sub};
-
-trait Scalar: Add + Div + Mul + Rem + Sub + std::marker::Sized + Clone {
-	type Item;
-	fn zero() -> Self::Item;
-	fn one() -> Self::Item;
-}
-
-impl Scalar for u32 {
-	type Item = u32;
-	fn zero() -> Self::Item {
-		0 as u32
-	}
-	fn one() -> Self::Item {
-		1 as u32
-	}
-}
+mod ops;
+mod scalar;
+use scalar::Scalar;
 
 #[derive(Debug, Eq, PartialEq)]
 struct Matrix<T>(Vec<Vec<T>>);
@@ -96,15 +70,6 @@ impl<T: Scalar<Item = T>> Matrix<T> {
 #[cfg(test)]
 mod tests {
 	use super::*;
-
-	#[test]
-	fn scalar() {
-		let a: u32 = u32::zero();
-		assert_eq!(a, 0 as u32);
-
-		let b = u32::one();
-		assert_eq!(b, 1 as u32);
-	}
 
 	#[test]
 	fn zero_property() {
