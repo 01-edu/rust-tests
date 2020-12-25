333e8fb4f08e3d5abefa5df4efb9e32729b9bba2
Augusto <aug.ornelas@gmail.com>
2020-12-25 22:30:04 +0000

    Fix solution for borrow exercise

--- borrow/src/lib.rs
+++ borrow/src/lib.rs
@@ -20,16 +20,15 @@ fn main() {
 ```
 */
 
+// fn main() {
+// 	let s = "hello";
+// 	let s1 = "camelCase".to_string();
 
-fn main() {
-	let s = "hello";
-	let s1 = "camelCase".to_string();
+// 	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
+// 	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
+// }
 
-	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
-	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
-}
-
-fn str_len(s: &str) -> usize {
+pub fn str_len(s: &str) -> usize {
 	// Write you're code here
 	s.len()
 }
