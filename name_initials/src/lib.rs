b46d5f534ee140b4e278b72c513928ef3c82ebca
Augusto <aug.ornelas@gmail.com>
2020-12-26 21:08:35 +0000

    Make the necessary functions public for the test of name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -10,9 +10,9 @@ with names and return a vector of Strings with the initials of each name.
 
 ```rust
 fn main() {
-    let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"]
-    println!("{:?}", initials(names));
-    // output: ["H. P.", "S. E.", "J. L.", "B. O."]
+	let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"]
+	println!("{:?}", initials(names));
+	// output: ["H. P.", "S. E.", "J. L.", "B. O."]
 }
 ```
 
@@ -25,81 +25,125 @@ fn main() {
 
 */
 
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-fn initials(names: &mut Vec<&str>) -> Vec<String> {
-    names.iter().map(|x| {
-        let mut two_names = x.split_whitespace();
-        let mut a = two_names.next().unwrap().chars().nth(0).unwrap().to_string();
-        a.push_str(". ");
-        a.push_str(&two_names.next().unwrap().chars().nth(0).unwrap().to_string());
-        a.push_str(".");
-        a
-    }).collect()
+pub fn initials(names: &mut Vec<&str>) -> Vec<String> {
+	names
+		.iter()
+		.map(|x| {
+			let mut two_names = x.split_whitespace();
+			let mut a = two_names
+				.next()
+				.unwrap()
+				.chars()
+				.nth(0)
+				.unwrap()
+				.to_string();
+			a.push_str(". ");
+			a.push_str(
+				&two_names
+					.next()
+					.unwrap()
+					.chars()
+					.nth(0)
+					.unwrap()
+					.to_string(),
+			);
+			a.push_str(".");
+			a
+		})
+		.collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use jemalloc_ctl::{epoch, stats};
+	use super::*;
+	use jemalloc_ctl::{epoch, stats};
 
-    struct Test<'a> {
-        names: Vec<&'a str>,
-        result: Vec<&'a str>
-    }
+	struct Test<'a> {
+		names: Vec<&'a str>,
+		result: Vec<&'a str>,
+	}
 
-    // solution that will run against the students solution
-    // this function uses the less heap allocation 
-    fn initials_sol(arr: &mut Vec<&str>) -> Vec<String> {
-        arr.iter().map(|ele| {
-            let mut names = ele.split_whitespace();
-            let mut a = names.next().unwrap().chars().nth(0).unwrap().to_string();
-            a.push_str(". ");
-            let mut b = names.next().unwrap().chars().nth(0).unwrap().to_string(); 
-            b.push_str(".");
-            a.push_str(&b);
-            a
-        }).collect()
-    }
+	// solution that will run against the students solution
+	// this function uses the less heap allocation
+	fn initials_sol(arr: &mut Vec<&str>) -> Vec<String> {
+		arr.iter()
+			.map(|ele| {
+				let mut names = ele.split_whitespace();
+				let mut a = names.next().unwrap().chars().nth(0).unwrap().to_string();
+				a.push_str(". ");
+				let mut b = names.next().unwrap().chars().nth(0).unwrap().to_string();
+				b.push_str(".");
+				a.push_str(&b);
+				a
+			})
+			.collect()
+	}
 
-    #[test]
-    fn test_memory_allocation() {
-        // the statistics tracked by jemalloc are cached
-        // The epoch controls when they are refreshed
-        let e = epoch::mib().unwrap();
-        // allocated: number of bytes allocated by the application
-        let allocated = stats::allocated::mib().unwrap();
-        let mut test_value = vec!["Lee Silva", "Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
-        
-        initials_sol(&mut test_value);
-        // this will advance with the epoch giving the its old value
-        // where we read the updated heap allocation using the `allocated.read()`
-        e.advance().unwrap();
-        let solution = allocated.read().unwrap();
+	#[test]
+	fn test_memory_allocation() {
+		// the statistics tracked by jemalloc are cached
+		// The epoch controls when they are refreshed
+		let e = epoch::mib().unwrap();
+		// allocated: number of bytes allocated by the application
+		let allocated = stats::allocated::mib().unwrap();
+		let mut test_value = vec![
+			"Lee Silva",
+			"Harry Potter",
+			"Someone Else",
+			"J. L.",
+			"Barack Obama",
+		];
 
-        initials(&mut test_value);
-        e.advance().unwrap();
-        let student = allocated.read().unwrap();
+		initials_sol(&mut test_value);
+		// this will advance with the epoch giving the its old value
+		// where we read the updated heap allocation using the `allocated.read()`
+		e.advance().unwrap();
+		let solution = allocated.read().unwrap();
 
-        assert!(student <= solution, format!("your heap allocation is {}, and it must be less or equal to {}", student, solution));
-    }
+		initials(&mut test_value);
+		e.advance().unwrap();
+		let student = allocated.read().unwrap();
 
-    #[test]
-    fn test_function() {
-        let cases = vec![
-            Test {
-                names: vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"],
-                result: vec!["H. P.", "S. E.", "J. L.", "B. O."]
-            },
-            Test {
-                names: vec!["James John", "David Joseph", "Matthew Brian", "Jacob Sousa", "Bruce Banner", "Scarlett Johansson", "Graydon Hoare"],
-                result: vec!["J. J.", "D. J.", "M. B.", "J. S.", "B. B.", "S. J.", "G. H."]
-            }
-        ];
+		assert!(
+			student <= solution,
+			format!(
+				"your heap allocation is {}, and it must be less or equal to {}",
+				student, solution
+			)
+		);
+	}
 
-        for mut v in cases {
-            assert_eq!(initials(&mut v.names), v.result.iter().map(|ele| ele.to_string()).collect::<Vec<String>>());
-        }
-    }
+	#[test]
+	fn test_function() {
+		let cases = vec![
+			Test {
+				names: vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"],
+				result: vec!["H. P.", "S. E.", "J. L.", "B. O."],
+			},
+			Test {
+				names: vec![
+					"James John",
+					"David Joseph",
+					"Matthew Brian",
+					"Jacob Sousa",
+					"Bruce Banner",
+					"Scarlett Johansson",
+					"Graydon Hoare",
+				],
+				result: vec![
+					"J. J.", "D. J.", "M. B.", "J. S.", "B. B.", "S. J.", "G. H.",
+				],
+			},
+		];
+
+		for mut v in cases {
+			assert_eq!(
+				initials(&mut v.names),
+				v.result
+					.iter()
+					.map(|ele| ele.to_string())
+					.collect::<Vec<String>>()
+			);
+		}
+	}
 }
