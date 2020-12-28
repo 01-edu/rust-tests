5ebeda65a64b92a8bdc91d4a5616c5d2f7941ad0
Augusto <aug.ornelas@gmail.com>
2020-12-28 21:12:39 +0000

    Make closures a library instead of a program

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -4,16 +4,16 @@
 
 use std::iter::Iterator;
 
-fn first_fifty_even_square() -> Vec<i32> {
+pub fn first_fifty_even_square() -> Vec<i32> {
 	(1..101).filter(|x| x % 2 == 0).map(|x| x * x).collect()
 }
 
-fn main() {
-	println!("Hello, world!");
-	let v1 = first_fifty_even_square();
+// fn main() {
+// 	println!("Hello, world!");
+// 	let v1 = first_fifty_even_square();
 
-	println!("All elements in {:?}, len = {}", v1, v1.len());
-}
+// 	println!("All elements in {:?}, len = {}", v1, v1.len());
+// }
 
 #[cfg(test)]
 mod tests {
