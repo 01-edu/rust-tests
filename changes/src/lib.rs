7df90ece588a6878b73aed06b687fa7d5ecac812
Augusto <aug.ornelas@gmail.com>
2021-01-05 14:23:21 +0000

    Change solution for new exercise `changes`

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -1,26 +1,54 @@
 /*
-## error types
+## changes
 
 ### Instructions
 
-Make this code compile
+Imagine you are working in a software to control smart lights in a
+house. You have access to an array of all the lights in a house.
+
+Define the associated function `new` to the data structure `Light`
+which creates a new light with the alias passed in the arguments and
+a brightness of 0.
+
+Define the function `change_brightness` that receives a Vec of lights,
+an alias and a u8 value and sets the u8 value as the new brightness of the light
+identified by the alias in the Vec of lights.
 
 */
 
 // It's not possible to have one mutable reference and one immutable
 // fn main() {
-// 	let ref mut a = String::from("Hello");
-// 	let len = a.len();
-// 	let b = &a;
-
-// 	add_excitement(&mut a.clone());
-
-// 	println!("The len of {} is {}", a, len);
-// 	println!("The length of {} is {}", b, b.len());
+// 	// bedroom
+// 	let mut lights = vec![
+// 		Light::new("living_room"),
+// 		Light::new("bedroom"),
+// 		Light::new("rest_room"),
+// 	];
+// 	println!("brightness = {}", lights[0].brightness);
+// 	change_brightness(&mut lights, "living_room", 200);
+// 	println!("new brightness = {}", lights[0].brightness);
 // }
 
-pub fn add_excitement(s: &mut String) {
-	s.push_str("!");
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	#[allow(dead_code)]
+	pub fn new(alias: &str) -> Self {
+		Self {
+			alias: alias.to_owned(),
+			brightness: 0,
+		}
+	}
+}
+
+pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
+	if let Some(mut light) = lights.into_iter().find(|x| x.alias == alias) {
+		light.brightness = value
+	}
 }
 
 #[cfg(test)]
@@ -28,19 +56,27 @@ mod test {
 	use super::*;
 
 	#[test]
-	fn test_ascii() {
-		let mut expected = "hello".to_string();
-		add_excitement(&mut expected);
-		assert_eq!("hello!", &expected);
-		let mut expected = "go on".to_string();
-		add_excitement(&mut expected);
-		assert_eq!("go on!", &expected);
+	fn test_unexistente_alias() {
+		let mut lights = Vec::new();
+		for i in 0..5 {
+			let alias = format!("light-{}", i);
+			lights.push(Light::new(&alias));
+		}
+		let copy = lights.clone();
+		change_brightness(&mut lights, "light-6", 100);
+		assert_eq!(copy, lights);
 	}
 
 	#[test]
-	fn test_unicode() {
-		let mut expected = "↕".to_string();
-		add_excitement(&mut expected);
-		assert_eq!(expected, "↕!");
+	fn test_alias() {
+		let mut lights = Vec::new();
+		for i in 0..5 {
+			let alias = format!("light-{}", i);
+			lights.push(Light::new(&alias));
+		}
+		let alias = "light-3";
+		change_brightness(&mut lights, alias, 100);
+		let light = lights.into_iter().find(|x| x.alias == alias).unwrap();
+		assert_eq!(light.brightness, 100);
 	}
 }
