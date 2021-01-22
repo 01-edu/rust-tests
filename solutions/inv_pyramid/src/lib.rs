900136ad2715ee1d63b039ec133e73cee5256a6c
lee <lee-dasilva@hotmail.com>
2021-01-22 15:48:34 +0000

    rust exercise (exam1): inv_pyramid

---
+++ solutions/inv_pyramid/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn inv_pyramid(st: String, i: u32) -> Vec<String> {
+    let mut vec = vec![];
+    for v in 1..i {
+        let mut s = String::from("");
+        for _ in 0..v {
+            s.push_str(" ")
+        }
+        for _ in 0..v {
+            s.push_str(&st)
+        }
+        vec.push(s);
+    }
+    for v in 0..i {
+        let mut s = String::from("");
+        for _ in 0..(i - v) {
+            s.push_str(" ")
+        }
+        for _ in 0..(i - v) {
+            s.push_str(&st)
+        }
+        vec.push(s);
+    }
+    return vec;
+}
