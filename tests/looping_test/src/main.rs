b0105511f3ccd07d26bbde45f14b5c60cddcd6fa
Augusto <aug.ornelas@gmail.com>
2021-02-10 23:06:21 +0000

    Add notification: The test is being and advice to do the next

--- tests/looping_test/src/main.rs
+++ tests/looping_test/src/main.rs
@@ -1,4 +1,4 @@
-use rexpect::spawn;
+//use rexpect::spawn;
 
 const MANIFEST_PATH: &str = "../../solutions/looping/Cargo.toml";
 
@@ -21,31 +21,35 @@ fn main() {
 }
 
 #[test]
-fn test_correct_answer() {
-	let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
-
-	let cmd = set_up();
-	let mut p = spawn(&cmd.path().display().to_string(), Some(2000)).unwrap();
-	p.exp_string(riddle).unwrap();
-	p.send_line("The letter e").unwrap();
-	p.exp_string("It took you 1 trials to get the right answer")
-		.unwrap();
+fn notication() {
+	panic!("This test is being refactored, please continue to the next exercise");
 }
+// #[test]
+// fn test_correct_answer() {
+// 	let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
 
-#[test]
-fn test_more_than_one_fail() {
-	let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+// 	let cmd = set_up();
+// 	let mut p = spawn(&cmd.path().display().to_string(), Some(2000)).unwrap();
+// 	p.exp_string(riddle).unwrap();
+// 	p.send_line("The letter e").unwrap();
+// 	p.exp_string("It took you 1 trials to get the right answer")
+// 		.unwrap();
+// }
 
-	let cmd = set_up();
-	let mut p = spawn(&cmd.path().display().to_string(), Some(2000)).unwrap();
-	p.exp_string(riddle).unwrap();
-	p.send_line("circle").unwrap();
-	p.exp_string(riddle).unwrap();
-	p.send_line("relativity").unwrap();
-	p.exp_string(riddle).unwrap();
-	p.send_line("the big bang").unwrap();
-	p.exp_string(riddle).unwrap();
-	p.send_line("The letter e").unwrap();
-	p.exp_string("It took you 4 trials to get the right answer")
-		.unwrap();
-}
+// #[test]
+// fn test_more_than_one_fail() {
+// 	let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+
+// 	let cmd = set_up();
+// 	let mut p = spawn(&cmd.path().display().to_string(), Some(2000)).unwrap();
+// 	p.exp_string(riddle).unwrap();
+// 	p.send_line("circle").unwrap();
+// 	p.exp_string(riddle).unwrap();
+// 	p.send_line("relativity").unwrap();
+// 	p.exp_string(riddle).unwrap();
+// 	p.send_line("the big bang").unwrap();
+// 	p.exp_string(riddle).unwrap();
+// 	p.send_line("The letter e").unwrap();
+// 	p.exp_string("It took you 4 trials to get the right answer")
+// 		.unwrap();
+// }
