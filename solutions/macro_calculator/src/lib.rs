dd8b972f19bcbd9b14f64bf25bb848d616437f92
OGordoo <marcosafonso8@hotmail.com>
2021-02-19 15:40:47 +0000

    switching crates from macro_calculator exercise

--- solutions/macro_calculator/src/lib.rs
+++ solutions/macro_calculator/src/lib.rs
@@ -39,187 +39,136 @@ Hint: You will need the `serde`, `serde_json` and `serde_derive` crates.
 
 ```rust
 fn main() {
-    let a = serde_json::json!(
-        {
-        "name": "light milk",
-        "calories": ["148kJ", "35kcal"],
-        "protein": 3.5,
-        "fats": 0.1,
-        "carbs": 5.0,
-        "nbr_of_portions": 0.7
-    });
+    let a = vec![
+        Food {
+            name: String::from("big mac"),
+            calories: ["2133.84kJ".to_string(), "510kcal".to_string()],
+            protein: 27.0,
+            fats: 26.0,
+            carbs: 41.0,
+            nbr_of_portions: 2.0,
+        },
+        Food {
+            name: "pizza margherita".to_string(),
+            calories: ["1500.59kJ".to_string(), "358.65kcal".to_string()],
+            protein: 13.89,
+            fats: 11.21,
+            carbs: 49.07,
+            nbr_of_portions: 4.9,
+        },
+    ];
 
-    let b = serde_json::json!({
-        "name": "oat cookies",
-        "calories": ["1996kJ", "477kcal"],
-        "protein": 8.2,
-        "fats": 21.0,
-        "carbs": 60.4,
-        "nbr_of_portions": 1.2,
-    });
-
-    let macros: Macros = serde_json::from_value(calculate_macros(vec![a, b])).unwrap();
-    println!("{:?}", macros);
+    println!("{:#}", calculate_macros(a));
 }
 ```
+
 */
 
-extern crate serde_json;
+extern crate json;
 
-#[macro_use]
-extern crate serde_derive;
+use json::object;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct Macros {
-    pub cals: f64,
-    pub carbs: f64,
-    pub proteins: f64,
+pub struct Food {
+    #[allow(dead_code)]
+    pub name: String,
+    pub calories: [String; 2],
+    pub protein: f64,
     pub fats: f64,
+    pub carbs: f64,
+    pub nbr_of_portions: f64,
 }
 
-pub fn calculate_macros(foods: Vec<serde_json::Value>) -> serde_json::Value {
+pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     let (mut cals, mut prot, mut carbs, mut fats) = (0.0, 0.0, 0.0, 0.0);
 
     for food in foods {
-        let cal = &food["calories"][1]
+        let cal = &food.calories[1]
             .to_string()
             .split("kcal")
             .collect::<Vec<&str>>()[0]
-            .split("\"")
-            .collect::<Vec<&str>>()[1]
             .to_string()
             .parse::<f64>()
             .unwrap();
 
-        cals += cal * food["nbr_of_portions"].as_f64().unwrap();
-        prot += food["protein"].as_f64().unwrap() * food["nbr_of_portions"].as_f64().unwrap();
-        carbs += food["carbs"].as_f64().unwrap() * food["nbr_of_portions"].as_f64().unwrap();
-        fats += food["fats"].as_f64().unwrap() * food["nbr_of_portions"].as_f64().unwrap();
+        cals += cal * food.nbr_of_portions;
+        prot += food.protein * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions;
+        fats += food.fats * food.nbr_of_portions;
     }
 
-    serde_json::json!({
+    object! {
         "cals": (cals * 100.0).round() / 100.0,
         "carbs": (carbs * 100.0).round() / 100.0,
         "proteins": (prot * 100.0).round() / 100.0,
         "fats": (fats * 100.0).round() / 100.0,
-    })
+    }
 }
-/*
-fn main() {
-    let a = serde_json::json!(
-        {
-        "name": "light milk",
-        "calories": ["148kJ", "35kcal"],
-        "protein": 3.5,
-        "fats": 0.1,
-        "carbs": 5.0,
-        "nbr_of_portions": 0.7
-    });
 
-    let b = serde_json::json!({
-        "name": "oat cookies",
-        "calories": ["1996kJ", "477kcal"],
-        "protein": 8.2,
-        "fats": 21.0,
-        "carbs": 60.4,
-        "nbr_of_portions": 1.2,
-    });
-
-    let macros: Macros = serde_json::from_value(calculate_macros(vec![a, b])).unwrap();
-    println!("{:?}", macros);
-}
-*/
 #[cfg(test)]
 mod test {
     use super::*;
 
-    impl PartialEq for Macros {
-        fn eq(&self, other: &Macros) -> bool {
-            self.carbs == other.carbs
-                && self.cals == other.cals
-                && self.proteins == other.proteins
-                && self.fats == other.fats
-        }
-    }
-
     #[test]
     fn testing_macros_values() {
-        let a = serde_json::json!(
-            {
-            "name": "light milk",
-            "calories": ["148kJ", "35kcal"],
-            "protein": 3.5,
-            "fats": 0.1,
-            "carbs": 5.0,
-            "nbr_of_portions": 0.7
-        });
-        let b = serde_json::json!({
-            "name": "oat cookies",
-            "calories": ["1996kJ", "477kcal"],
-            "protein": 8.2,
-            "fats": 21.0,
-            "carbs": 60.4,
-            "nbr_of_portions": 1.2,
-        });
+        let a = Food {
+            name: "light milk".to_string(),
+            calories: ["148kJ".to_string(), "35kcal".to_string()],
+            protein: 3.5,
+            fats: 0.1,
+            carbs: 5.0,
+            nbr_of_portions: 0.7,
+        };
+        let b = Food {
+            name: "oat cookies".to_string(),
+            calories: ["1996kJ".to_string(), "477kcal".to_string()],
+            protein: 8.2,
+            fats: 21.0,
+            carbs: 60.4,
+            nbr_of_portions: 1.2,
+        };
 
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![a, b])).unwrap();
+        let macros = calculate_macros(vec![a, b]);
 
-        assert_eq!(
-            Macros {
-                cals: 596.9,
-                carbs: 75.98,
-                proteins: 12.29,
-                fats: 25.27
-            },
-            macros
-        );
+        assert_eq!(macros["cals"], 596.9);
+        assert_eq!(macros["carbs"], 75.98);
+        assert_eq!(macros["proteins"], 12.29);
+        assert_eq!(macros["fats"], 25.27);
     }
 
     #[test]
     fn testing_no_food() {
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![])).unwrap();
+        let macros = calculate_macros(vec![]);
 
-        assert_eq!(
-            Macros {
-                cals: 0.0,
-                carbs: 0.0,
-                proteins: 0.0,
-                fats: 0.0
-            },
-            macros
-        );
+        assert_eq!(macros["cals"], 0.0);
+        assert_eq!(macros["carbs"], 0.0);
+        assert_eq!(macros["proteins"], 0.0);
+        assert_eq!(macros["fats"], 0.0);
     }
 
     #[test]
     fn big_values() {
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![
-            serde_json::json!({
-                "name": "big mac",
-                "calories": ["2133.84kJ", "510kcal"],
-                "protein": 27,
-                "fats": 26,
-                "carbs": 41,
-                "nbr_of_portions": 2,
-            }),
-            serde_json::json!({
-                "name": "pizza margherita",
-                "calories": ["1500.59kJ", "358.65kcal"],
-                "protein": 13.89,
-                "fats": 11.21,
-                "carbs": 49.07,
-                "nbr_of_portions": 4.9,
-            }),
-        ]))
-        .unwrap();
-
-        assert_eq!(
-            Macros {
-                cals: 2777.39,
-                carbs: 322.44,
-                proteins: 122.06,
-                fats: 106.93
+        let macros = calculate_macros(vec![
+            Food {
+                name: "big mac".to_string(),
+                calories: ["2133.84kJ".to_string(), "510kcal".to_string()],
+                protein: 27.0,
+                fats: 26.0,
+                carbs: 41.0,
+                nbr_of_portions: 2.0,
             },
-            macros
-        );
+            Food {
+                name: "pizza margherita".to_string(),
+                calories: ["1500.59kJ".to_string(), "358.65kcal".to_string()],
+                protein: 13.89,
+                fats: 11.21,
+                carbs: 49.07,
+                nbr_of_portions: 4.9,
+            },
+        ]);
+
+        assert_eq!(macros["cals"], 2777.39);
+        assert_eq!(macros["carbs"], 322.44);
+        assert_eq!(macros["proteins"], 122.06);
+        assert_eq!(macros["fats"], 106.9);
     }
 }
--- tests/macro_calculator_test/src/main.rs
+++ tests/macro_calculator_test/src/main.rs
@@ -1,27 +1,27 @@
+extern crate json;
 use macro_calculator::*;
 
 fn main() {
-    let a = serde_json::json!(
-        {
-        "name": "light milk",
-        "calories": ["148kJ", "35kcal"],
-        "protein": 3.5,
-        "fats": 0.1,
-        "carbs": 5.0,
-        "nbr_of_portions": 0.7
-    });
+    let foods = vec![
+        Food {
+            name: String::from("big mac"),
+            calories: ["2133.84kJ".to_string(), "510kcal".to_string()],
+            protein: 27.0,
+            fats: 26.0,
+            carbs: 41.0,
+            nbr_of_portions: 2.0,
+        },
+        Food {
+            name: "pizza margherita".to_string(),
+            calories: ["1500.59kJ".to_string(), "358.65kcal".to_string()],
+            protein: 13.89,
+            fats: 11.21,
+            carbs: 49.07,
+            nbr_of_portions: 4.9,
+        },
+    ];
 
-    let b = serde_json::json!({
-        "name": "oat cookies",
-        "calories": ["1996kJ", "477kcal"],
-        "protein": 8.2,
-        "fats": 21.0,
-        "carbs": 60.4,
-        "nbr_of_portions": 1.2,
-    });
-
-    let macros: Macros = serde_json::from_value(calculate_macros(vec![a, b])).unwrap();
-    println!("{:?}", macros);
+    println!("{:#}", calculate_macros(foods));
 }
 
 #[cfg(test)]
@@ -30,82 +30,65 @@ mod test {
 
     #[test]
     fn testing_macros_values() {
-        let a = serde_json::json!(
-            {
-            "name": "light milk",
-            "calories": ["148kJ", "35kcal"],
-            "protein": 3.5,
-            "fats": 0.1,
-            "carbs": 5.0,
-            "nbr_of_portions": 0.7
-        });
-        let b = serde_json::json!({
-            "name": "oat cookies",
-            "calories": ["1996kJ", "477kcal"],
-            "protein": 8.2,
-            "fats": 21.0,
-            "carbs": 60.4,
-            "nbr_of_portions": 1.2,
-        });
+        let a = Food {
+            name: "light milk".to_string(),
+            calories: ["148kJ".to_string(), "35kcal".to_string()],
+            protein: 3.5,
+            fats: 0.1,
+            carbs: 5.0,
+            nbr_of_portions: 0.7,
+        };
+        let b = Food {
+            name: "oat cookies".to_string(),
+            calories: ["1996kJ".to_string(), "477kcal".to_string()],
+            protein: 8.2,
+            fats: 21.0,
+            carbs: 60.4,
+            nbr_of_portions: 1.2,
+        };
 
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![a, b])).unwrap();
+        let macros = calculate_macros(vec![a, b]);
 
-        assert_eq!(
-            Macros {
-                cals: 596.9,
-                carbs: 75.98,
-                proteins: 12.29,
-                fats: 25.27
-            },
-            macros
-        );
+        assert_eq!(macros["cals"].as_f64().unwrap(), 596.9);
+        assert_eq!(macros["carbs"].as_f64().unwrap(), 75.98);
+        assert_eq!(macros["proteins"].as_f64().unwrap(), 12.29);
+        assert_eq!(macros["fats"].as_f64().unwrap(), 25.27);
     }
 
     #[test]
     fn testing_no_food() {
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![])).unwrap();
+        let macros = calculate_macros(vec![]);
 
-        assert_eq!(
-            Macros {
-                cals: 0.0,
-                carbs: 0.0,
-                proteins: 0.0,
-                fats: 0.0
-            },
-            macros
-        );
+        assert_eq!(macros["cals"].as_f64().unwrap(), 0.0);
+        assert_eq!(macros["carbs"].as_f64().unwrap(), 0.0);
+        assert_eq!(macros["proteins"].as_f64().unwrap(), 0.0);
+        assert_eq!(macros["fats"].as_f64().unwrap(), 0.0);
     }
 
     #[test]
     fn big_values() {
-        let macros: Macros = serde_json::from_value(calculate_macros(vec![
-            serde_json::json!({
-                "name": "big mac",
-                "calories": ["2133.84kJ", "510kcal"],
-                "protein": 27,
-                "fats": 26,
-                "carbs": 41,
-                "nbr_of_portions": 2,
-            }),
-            serde_json::json!({
-                "name": "pizza margherita",
-                "calories": ["1500.59kJ", "358.65kcal"],
-                "protein": 13.89,
-                "fats": 11.21,
-                "carbs": 49.07,
-                "nbr_of_portions": 4.9,
-            }),
-        ]))
-        .unwrap();
-
-        assert_eq!(
-            Macros {
-                cals: 2777.39,
-                carbs: 322.44,
-                proteins: 122.06,
-                fats: 106.93
+        let macros = calculate_macros(vec![
+            Food {
+                name: "big mac".to_string(),
+                calories: ["2133.84kJ".to_string(), "510kcal".to_string()],
+                protein: 27.0,
+                fats: 26.0,
+                carbs: 41.0,
+                nbr_of_portions: 2.0,
             },
-            macros
-        );
+            Food {
+                name: "pizza margherita".to_string(),
+                calories: ["1500.59kJ".to_string(), "358.65kcal".to_string()],
+                protein: 13.89,
+                fats: 11.21,
+                carbs: 49.07,
+                nbr_of_portions: 4.9,
+            },
+        ]);
+
+        assert_eq!(macros["cals"].as_f64().unwrap(), 2777.39);
+        assert_eq!(macros["carbs"].as_f64().unwrap(), 322.44);
+        assert_eq!(macros["proteins"].as_f64().unwrap(), 122.06);
+        assert_eq!(macros["fats"].as_f64().unwrap(), 106.93);
     }
 }
