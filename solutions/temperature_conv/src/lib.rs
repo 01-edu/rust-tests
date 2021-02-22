8675d83704e1aad3c68697840eae8326d4ef3a50
Augusto <aug.ornelas@gmail.com>
2021-02-22 10:03:25 +0000

    Fix test for the exercise temperature_conv

--- solutions/temperature_conv/src/lib.rs
+++ solutions/temperature_conv/src/lib.rs
@@ -11,25 +11,27 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
 	c * 1.8 + 32.0
 }
 
-#[cfg(test)]
-mod tests {
+#[cfg(tests)]
+mod test {
 	use super::*;
-	use std::f64::EPSILON;
-
+	#[allow(dead_code)]
 	fn eql(a: f64, b: f64) -> bool {
-		(b - a).abs() < EPSILON
+		(b - a).abs() < std::f64::EPSILON
 	}
 
 	#[test]
 	fn test_f_to_c() {
-		println!("{}", fahrenheit_to_celsius(83.0));
+		println!("{}°F = {}°C", 20.0, fahrenheit_to_celsius(20.0));
 		assert!(eql(fahrenheit_to_celsius(20.0), -6.666666666666666));
+		println!("{}°F = {}°C", 83.0, fahrenheit_to_celsius(83.0));
 		assert!(eql(fahrenheit_to_celsius(83.0), 28.333333333333332));
 	}
 
 	#[test]
 	fn test_c_to_f() {
+		println!("{}°C = {}°F", 27.0, fahrenheit_to_celsius(27.0));
 		assert!(eql(celsius_to_fahrenheit(27.0), 80.6));
+		println!("{}°F = {}°C", 0.0, fahrenheit_to_celsius(32.0));
 		assert!(eql(celsius_to_fahrenheit(0.0), 32.0))
 	}
 }
--- tests/temperature_conv_test/src/main.rs
+++ tests/temperature_conv_test/src/main.rs
@@ -8,13 +8,16 @@ fn eql(a: f64, b: f64) -> bool {
 
 #[test]
 fn test_f_to_c() {
-	println!("{}", fahrenheit_to_celsius(83.0));
+	println!("{}°F = {}°C", 20.0, fahrenheit_to_celsius(20.0));
 	assert!(eql(fahrenheit_to_celsius(20.0), -6.666666666666666));
+	println!("{}°F = {}°C", 83.0, fahrenheit_to_celsius(83.0));
 	assert!(eql(fahrenheit_to_celsius(83.0), 28.333333333333332));
 }
 
 #[test]
 fn test_c_to_f() {
+	println!("{}°C = {}°F", 27.0, fahrenheit_to_celsius(27.0));
 	assert!(eql(celsius_to_fahrenheit(27.0), 80.6));
+	println!("{}°F = {}°C", 0.0, fahrenheit_to_celsius(32.0));
 	assert!(eql(celsius_to_fahrenheit(0.0), 32.0))
 }
