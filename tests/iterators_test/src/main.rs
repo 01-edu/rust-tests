a8c18eeedda5f7b27db5bf7765105508b07315f8
MSilva95 <miguel-silva98@hotmail.com>
2021-11-05 17:16:31 +0000

    changing the test acording to the new version of the exercise iterrators

--- tests/iterators_test/src/main.rs
+++ tests/iterators_test/src/main.rs
@@ -4,15 +4,17 @@
 ### Instructions
 
 Create a method `new` that takes one number `usize` and initializes the `Number` struct.
-This method will have to determinate if the given number is even or odd,
-if it is even you will have to increment one to the odd number and
-if it is odd you have to increment one to the even number.
+This method will have to determinate if the given number is even or odd.
 
-After that you will implement an iterator for the struct `Number` that returns a tuple (usize,usize,usize).
-containing each field of the struct Number.
-The first position of the tuple will be the even numbers,
-the second will be the odd numbers,
-and the third will be the factorial numbers.
+After that you will implement an iterator for the struct `Number` that returns a tuple (usize,usize,usize) containing each field of the struct Number.
+
+The first position of the tuple will have the even numbers, the second will have the odd numbers, and the third will have the factorial numbers.
+
+That being said, You will have to follow this rules:
+
+- If the number is even you will have to calculate the factorial of the next odd number.
+- If the number is odd you will have to calculate the factorial of the next even number.
+- The tupple has to return all the numbers in the right positions.
 
 ### Notions
 
@@ -22,15 +24,13 @@ and the third will be the factorial numbers.
 use iterators::*;
 
 fn main() {
-    let mut a = Number::new(5);
-    println!("{:?}", a.next()); // Some((6, 5, 120))
-    println!("{:?}", a.next()); // Some((8, 7, 720))
-    println!("{:?}", a.next()); // Some((10, 9, 5040))
-    println!();
-    let mut a = Number::new(18);
-    println!("{:?}", a.next()); // Some((18, 19, 6402373705728000))
-    println!("{:?}", a.next()); // Some((20, 21, 121645100408832000))
-    println!("{:?}", a.next()); // Some((22, 23, 2432902008176640000))
+    let mut a = Number::new(4);
+    println!("{:?}", a.next());
+    println!("{:?}", a.next());
+    println!("{:?}", a.next());
+    println!("{:?}", a.next());
+    println!("{:?}", a.next());
+    println!("{:?}", a.next());
 }
 
 #[cfg(test)]
@@ -38,27 +38,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_first_seven() {
-        let test_even = vec![0, 2, 4, 6, 8, 10, 12];
-        let test_odd = vec![1, 3, 5, 7, 9, 11, 13];
-        let test_fact = vec![1, 1, 2, 6, 24, 120, 720];
-
-        // for (i, x) in Number::new(0).take(7).enumerate() {
-        //     assert_eq!(x.0, test_even[i]);
-        //     assert_eq!(x.1, test_odd[i]);
-        //     assert_eq!(x.2, test_fact[i]);
-        // }
-        assert_eq!(1 + 1, 2);
-    }
-
-    #[test]
     fn test_next() {
-        let mut a = Number::new(6);
-        // assert_eq!(a.next().unwrap(), (6, 7, 720));
-        // assert_eq!(a.next().unwrap(), (8, 9, 5040));
-        // assert_eq!(a.next().unwrap(), (10, 11, 40320));
-        // assert_eq!(a.next().unwrap(), (12, 13, 362880));
-        // assert_eq!(a.next().unwrap(), (14, 15, 3628800));
-        assert_eq!(1 + 1, 2);
+        let mut a = Number::new(4);
+        assert_eq!(a.next().unwrap(), (6, 5, 720));
+        assert_eq!(a.next().unwrap(), (6, 7, 5040));
+        assert_eq!(a.next().unwrap(), (8, 7, 40320));
+        assert_eq!(a.next().unwrap(), (8, 9, 362880));
+        assert_eq!(a.next().unwrap(), (10, 9, 3628800));
     }
 }
