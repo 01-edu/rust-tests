81b54d810e37108b2aafce2cde89036210cb80b6
Augusto <aug.ornelas@gmail.com>
2021-11-16 17:44:05 +0000

    Add better error messages for exercise `matrix_mult`

--- solutions/card_deck/src/lib.rs
+++ solutions/card_deck/src/lib.rs
@@ -1,67 +1,67 @@
 // Create a enum that represent the card suits
 use rand::Rng;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Suit {
-	Heart,
-	Diamond,
-	Spade,
-	Club,
+    Heart,
+    Diamond,
+    Spade,
+    Club,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Rank {
-	Ace,
-	King,
-	Queen,
-	Jack,
-	Number(u8),
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Number(u8),
 }
 
 impl Rank {
-	pub fn random() -> Rank {
-		let value: u8 = rand::thread_rng().gen_range(1, 14);
-		Rank::translate(value)
-	}
+    pub fn random() -> Rank {
+        let value: u8 = rand::thread_rng().gen_range(1, 14);
+        Rank::translate(value)
+    }
 
-	pub fn translate(value: u8) -> Rank {
-		match value {
-			1 => Rank::Ace,
-			n @ 2..=10 => Rank::Number(n),
-			11 => Rank::Jack,
-			12 => Rank::Queen,
-			_ => Rank::King,
-		}
-	}
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            n @ 2..=10 => Rank::Number(n),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            _ => Rank::King,
+        }
+    }
 }
 
 impl Suit {
-	pub fn random() -> Suit {
-		let value = rand::thread_rng().gen_range(1, 5);
-		Suit::translate(value)
-	}
+    pub fn random() -> Suit {
+        let value = rand::thread_rng().gen_range(1, 5);
+        Suit::translate(value)
+    }
 
-	pub fn translate(value: u8) -> Suit {
-		match value {
-			1 => Suit::Heart,
-			2 => Suit::Diamond,
-			3 => Suit::Spade,
-			_ => Suit::Club,
-		}
-	}
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Card {
-	pub suit: Suit,
-	pub rank: Rank,
+    pub suit: Suit,
+    pub rank: Rank,
 }
 
-pub fn winner_card(card: Card) -> bool {
-	Card {
-		suit: Suit::Spade,
-		rank: Rank::Ace,
-	} == card
+pub fn winner_card(card: &Card) -> bool {
+    Card {
+        suit: Suit::Spade,
+        rank: Rank::Ace,
+    } == *card
 }
 // Write a program that takes that returns a random card in the deck
 // A standard deck of cards has 52 cards: 4 suits and 13 cards per suit
@@ -81,26 +81,26 @@ pub fn winner_card(card: Card) -> bool {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_winner() {
-		let winner = Card {
-			rank: Rank::Ace,
-			suit: Suit::Spade,
-		};
-		for rank in 1..14 {
-			for suit in 1..5 {
-				let card = Card {
-					rank: Rank::translate(rank),
-					suit: Suit::translate(suit),
-				};
-				if card != winner {
-					assert!(!winner_card(card));
-				} else {
-					assert!(winner_card(card));
-				}
-			}
-		}
-	}
+    #[test]
+    fn test_winner() {
+        let winner = Card {
+            rank: Rank::Ace,
+            suit: Suit::Spade,
+        };
+        for rank in 1..14 {
+            for suit in 1..5 {
+                let card = Card {
+                    rank: Rank::translate(rank),
+                    suit: Suit::translate(suit),
+                };
+                if card != winner {
+                    assert!(!winner_card(card));
+                } else {
+                    assert!(winner_card(card));
+                }
+            }
+        }
+    }
 }
