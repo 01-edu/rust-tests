da1550ab23192f135442732379a1432d55328567
davidrobert99 <davidedamata@gmail.com>
2022-03-03 10:43:28 +0000

    changed to i32 and check the abs() of the log function

--- solutions/copy/src/lib.rs
+++ solutions/copy/src/lib.rs
@@ -33,14 +33,14 @@ fn main() {
 - https://doc.rust-lang.org/rust-by-example/scope/move.html
 */
 
-pub fn nbr_function(c: u32) -> (u32, f64, f64) {
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
 	(c, exponential(c), logarithm(c))
 }
 
-pub fn logarithm(n: u32) -> f64 {
-	(n as f64).ln().abs()
+pub fn logarithm(n: i32) -> f64 {
+	(n.abs() as f64).ln()
 }
-pub fn exponential(n: u32) -> f64 {
+pub fn exponential(n: i32) -> f64 {
 	(n as f64).exp()
 }
 
@@ -50,12 +50,12 @@ pub fn str_function(a: String) -> (String, String) {
 pub fn exp(s: String) -> Vec<String> {
 	let mut v: Vec<String> = Vec::new();
 	for token in s.split_whitespace() {
-		v.push(exponential(token.parse::<u32>().unwrap()).to_string());
+		v.push(exponential(token.parse::<i32>().unwrap()).to_string());
 	}
 	v
 }
 
-pub fn vec_function(b: Vec<u32>) -> (Vec<u32>, Vec<f64>) {
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
 	(b.clone(), b.iter().map(|v| logarithm(*v)).collect())
 }
 
@@ -70,8 +70,19 @@ mod tests {
 			(12, 162754.79141900392, 2.4849066497880004)
 		);
 		assert_eq!(nbr_function(1), (1, 2.718281828459045, 0.0));
-		assert_eq!(nbr_function(0), (0, 1.0, std::f64::INFINITY));
+		assert_eq!(nbr_function(0), (0, 1.0, std::f64::NEG_INFINITY));
 	}
+
+	#[test]
+	fn negative_nbr_test() {
+		assert_eq!(
+			nbr_function(-12),
+			(-12, 0.00000614421235332821, 2.4849066497880004)
+		);
+		assert_eq!(nbr_function(-1), (-1, 0.36787944117144233, 0.0));
+		assert_eq!(nbr_function(0), (0, 1.0, std::f64::NEG_INFINITY));
+	}
+
 	#[test]
 	fn ownership_vec_test() {
 		assert_eq!(
@@ -81,10 +92,12 @@ mod tests {
 				vec![0.0, 0.6931471805599453, 1.3862943611198906]
 			)
 		);
+
 		assert_eq!(
 			vec_function(vec![0, 1]),
-			(vec![0, 1], vec![std::f64::INFINITY, 0.0])
+			(vec![0, 1], vec![std::f64::NEG_INFINITY, 0.0])
 		);
+
 		assert_eq!(
 			vec_function(vec![1, 2, 4, 5]),
 			(
@@ -98,6 +111,7 @@ mod tests {
 			)
 		);
 	}
+
 	#[test]
 	fn ownership_str_test() {
 		assert_eq!(
@@ -112,7 +126,7 @@ mod tests {
 			(("1 0".to_string(), "2.718281828459045 1".to_string()))
 		);
 		assert_eq!(str_function(
-            String::from("1 2 4 5 6")),
-            (("1 2 4 5 6".to_string(), "2.718281828459045 7.38905609893065 54.598150033144236 148.4131591025766 403.4287934927351".to_string())));
+				String::from("1 2 4 5 6")),
+				(("1 2 4 5 6".to_string(), "2.718281828459045 7.38905609893065 54.598150033144236 148.4131591025766 403.4287934927351".to_string())));
 	}
 }
