dad84ad23fdc9cb6ebc30d4971db6055cc442524
davidrobert99 <davidedamata@gmail.com>
2022-03-29 09:47:28 +0100

    project motion update

--- solutions/project_motion/src/lib.rs
+++ solutions/project_motion/src/lib.rs
@@ -3,93 +3,87 @@
 
 ### Instructions
 
-For this exercise you will have to create a [projectile motion](https://cimg2.ck12.org/datastreams/f-d%3Abb024be6673110b31e78b46819e792adaed8dc661e082a61f0a6d64e%2BIMAGE%2BIMAGE.1).
+For this exercise, you will have to create a [projectile motion](https://cimg2.ck12.org/datastreams/f-d%3Abb024be6673110b31e78b46819e792adaed8dc661e082a61f0a6d64e%2BIMAGE%2BIMAGE.1).
 
-You will be provided with a structure called `Object` that will have all variables that are
-essential for the projectile physics. (distance, velocity, height, time)
+Two structures will be provided. A structure called `Object` which will have the corresponding values of X and Y.
+
+A structure called `ThrowObject` that will contain all the variables that are
+essential for the projectile physics (initial position, initial velocity, current position, current velocity and time).
 
 You must implement :
 
-- A function `throw_object` that will initialize the Object with a given velocity and height.
-- The trait Iterator with the `.next()` in which it must calculate the next position of the object after 1 second.
-  It will return an `Option` with the Object, It will return `None` if the object already reached the floor.
-
-### Example
-
-```rust
-fn main() {
-    let mut obj = Object::throw_object(50.0, 150.0);
-    println!("{:?}", obj.next());
-    println!("{:?}", obj.next());
-    println!("{:?}", obj.next());
-    println!("{:?}", obj.next());
-    println!("{:?}", obj.next());
-    println!("{:?}", obj.next());
-    // output:
-    // Some(Object { distance: 50.0, velocity: 50.0, height: 145.1, time: 1.0 })
-    // Some(Object { distance: 100.0, velocity: 50.0, height: 125.5, time: 2.0 })
-    // Some(Object { distance: 150.0, velocity: 50.0, height: 81.4, time: 3.0 })
-    // Some(Object { distance: 200.0, velocity: 50.0, height: 3.0, time: 4.0 })
-    // None
-    // None
-}
-```
+- A function `new` that will initialize the ThrowObject with a given initial position and an initial velocity.
+- The trait Iterator with the `.next()` in which the position and speed of the object must be calculated after 1 second.
+  It will return an `Option` with the ThrowObject, or it will return `None` if the ThrowObject has already reached the floor.
 
 ### Notions
 
-- https://doc.rust-lang.org/std/iter/trait.Iterator.html
-- https://doc.rust-lang.org/rust-by-example/trait/iter.html
+- [trait Iterator](https://doc.rust-lang.org/std/iter/trait.Iterator.html)
+- [iter](https://doc.rust-lang.org/rust-by-example/trait/iter.html)
 
 */
 
-// use std::iter::Sum;
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Object {
-    pub distance: f32,
-    pub velocity: f32,
-    pub height: f32,
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ThrowObject {
+    pub init_position: Object,
+    pub init_velocity: Object,
+    pub actual_position: Object,
+    pub actual_velocity: Object,
     pub time: f32,
 }
 
-impl Object {
-    pub fn throw_object(velocity: f32, height: f32) -> Object {
-        Object { distance: 0.0, velocity, height, time: 0.0 }
+impl ThrowObject {
+    pub fn new(init_position: Object, init_velocity: Object) -> ThrowObject {
+        ThrowObject {
+            init_position: init_position.clone(),
+            init_velocity: init_velocity.clone(),
+            actual_position: init_position.clone(),
+            actual_velocity: init_velocity.clone(),
+            time: 0.0,
+        }
     }
 }
 
-impl Iterator for Object {
-    type Item = Object;
+impl Iterator for ThrowObject {
+    type Item = ThrowObject;
 
-    // next will give the next position of the projectile
-    fn next(&mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<ThrowObject> {
         self.time += 1.0;
-        self.distance = round_two(self.time * self.velocity);
-        self.velocity = round_two(self.distance/self.time);
-        self.height = round_two(self.height - (9.8 * self.time.powf(2.0)) / 2.0);
+        let actual_distance = Object {
+            x: round_two(self.init_position.x + self.init_velocity.x * self.time),
+            y: round_two(
+                self.init_position.y + self.init_velocity.y * self.time
+                    - 9.8 * self.time * self.time / 2.0,
+            ),
+        };
 
-        if self.height <= 0.0 {
+        let actual_velocity = Object {
+            x: round_two(self.init_velocity.x),
+            y: round_two(self.init_velocity.y - 9.8 * self.time),
+        };
+
+        if actual_distance.y < 0.0 {
             return None;
+        } else {
+            return Some(ThrowObject {
+                init_position: self.init_position.clone(),
+                init_velocity: self.init_velocity.clone(),
+                actual_position: actual_distance,
+                actual_velocity: actual_velocity,
+                time: self.time,
+            });
         }
-        Some(self.clone())
     }
-
-    // - filter -> it return an array with the remaining distances and velocities, depending on the **fn given**
-    // // range final range of the projectile
-    // fn sum<S>(self) -> S
-    // where
-    //     S: Sum<Self::Item>,
-    // {
-    //     self.distance = self.velocity * ((2.0*self.height)/9.8).sqrt();
-    //     self.height = 0.0;
-    //     self.time = self.distance/self.velocity;
-
-    //     S::sum(iter::once(self))
-    // }
 }
 
 fn round_two(nbr: f32) -> f32 {
-    (nbr*100.0).round() / 100.0
+    (nbr * 100.0).round() / 100.0
 }
 
 #[cfg(test)]
@@ -97,42 +91,197 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_short_distance() {
-        let mut object = Object::throw_object(50.0, 20.0);
+    fn test_without_velocity() {
+        let mut obj = ThrowObject::new(Object { x: 50.0, y: 50.0 }, Object { x: 0.0, y: 0.0 });
 
-        assert_eq!(object.next(), Some(Object { distance: 50.0, velocity: 50.0, height: 15.1, time: 1.0 }));
-        assert_eq!(object, Object { distance: 50.0, velocity: 50.0, height: 15.1, time: 1.0 });
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 50.0, y: 50.0 },
+                init_velocity: Object { x: 0.0, y: 0.0 },
+                actual_position: Object { x: 50.0, y: 45.1 },
+                actual_velocity: Object { x: 0.0, y: -9.8 },
+                time: 1.0,
+            })
+        );
 
-        assert!(object.next().is_none(), "{:?} instead of None", object);
-        assert!(object.next().is_none(), "{:?} instead of None", object);
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 50.0, y: 50.0 },
+                init_velocity: Object { x: 0.0, y: 0.0 },
+                actual_position: Object { x: 50.0, y: 30.4 },
+                actual_velocity: Object { x: 0.0, y: -19.6 },
+                time: 2.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 50.0, y: 50.0 },
+                init_velocity: Object { x: 0.0, y: 0.0 },
+                actual_position: Object { x: 50.0, y: 5.9 },
+                actual_velocity: Object { x: 0.0, y: -29.4 },
+                time: 3.0,
+            })
+        );
+
+        assert!(obj.next().is_none(), "{:?} instead of None", obj);
+
+        assert!(obj.next().is_none(), "{:?} instead of None", obj);
     }
 
     #[test]
-    fn test_media_distance() {
-        let mut object = Object::throw_object(100.0, 30.0);
+    fn test_with_velocity() {
+        let mut obj = ThrowObject::new(Object { x: 0.0, y: 50.0 }, Object { x: 10.0, y: 10.0 });
 
-        assert_eq!(object.next(), Some(Object { distance: 100.0, velocity: 100.0, height: 25.1, time: 1.0 }));
-        assert_eq!(object, Object { distance: 100.0, velocity: 100.0, height: 25.1, time: 1.0 });
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 50.0 },
+                init_velocity: Object { x: 10.0, y: 10.0 },
+                actual_position: Object { x: 10.0, y: 55.1 },
+                actual_velocity: Object { x: 10.0, y: 0.2 },
+                time: 1.0,
+            })
+        );
 
-        assert_eq!(object.next(), Some(Object { distance: 200.0, velocity: 100.0, height: 5.5, time: 2.0 }));
-        assert_eq!(object, Object { distance: 200.0, velocity: 100.0, height: 5.5, time: 2.0 });
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 50.0 },
+                init_velocity: Object { x: 10.0, y: 10.0 },
+                actual_position: Object { x: 20.0, y: 50.4 },
+                actual_velocity: Object { x: 10.0, y: -9.6 },
+                time: 2.0,
+            })
+        );
 
-        assert!(object.next().is_none(), "{:?} instead of None", object);
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 50.0 },
+                init_velocity: Object { x: 10.0, y: 10.0 },
+                actual_position: Object { x: 30.0, y: 35.9 },
+                actual_velocity: Object { x: 10.0, y: -19.4 },
+                time: 3.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 50.0 },
+                init_velocity: Object { x: 10.0, y: 10.0 },
+                actual_position: Object { x: 40.0, y: 11.6 },
+                actual_velocity: Object { x: 10.0, y: -29.2 },
+                time: 4.0,
+            })
+        );
+
+        assert!(obj.next().is_none(), "{:?} instead of None", obj);
     }
 
     #[test]
-    fn test_long_distance() {
-        let mut object = Object::throw_object(120.0, 100.0);
+    fn test_with_negative_velocity() {
+        let mut obj = ThrowObject::new(Object { x: -10.0, y: 50.0 }, Object { x: -10.0, y: -10.0 });
 
-        assert_eq!(object.next(), Some(Object { distance: 120.0, velocity: 120.0, height: 95.1, time: 1.0 }));
-        assert_eq!(object, Object { distance: 120.0, velocity: 120.0, height: 95.1, time: 1.0 });
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: -10.0, y: 50.0 },
+                init_velocity: Object { x: -10.0, y: -10.0 },
+                actual_position: Object { x: -20.0, y: 35.1 },
+                actual_velocity: Object { x: -10.0, y: -19.8 },
+                time: 1.0,
+            })
+        );
 
-        assert_eq!(object.next(), Some(Object { distance: 240.0, velocity: 120.0, height: 75.5, time: 2.0 }));
-        assert_eq!(object, Object { distance: 240.0, velocity: 120.0, height: 75.5, time: 2.0 });
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: -10.0, y: 50.0 },
+                init_velocity: Object { x: -10.0, y: -10.0 },
+                actual_position: Object { x: -30.0, y: 10.4 },
+                actual_velocity: Object { x: -10.0, y: -29.6 },
+                time: 2.0,
+            })
+        );
 
-        assert_eq!(object.next(), Some(Object { distance: 360.0, velocity: 120.0, height: 31.4, time: 3.0 }));
-        assert_eq!(object, Object { distance: 360.0, velocity: 120.0, height: 31.4, time: 3.0 });
-        
-        assert!(object.next().is_none(), "{:?} instead of None", object);
+        assert!(obj.next().is_none(), "{:?} instead of None", obj);
+    }
+
+    #[test]
+    fn test_Long_distance() {
+        let mut obj = ThrowObject::new(Object { x: 0.0, y: 150.0 }, Object { x: 0.0, y: 10.0 });
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 155.1 },
+                actual_velocity: Object { x: 0.0, y: 0.2 },
+                time: 1.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 150.4 },
+                actual_velocity: Object { x: 0.0, y: -9.6 },
+                time: 2.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 135.9 },
+                actual_velocity: Object { x: 0.0, y: -19.4 },
+                time: 3.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 111.6 },
+                actual_velocity: Object { x: 0.0, y: -29.2 },
+                time: 4.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 77.5 },
+                actual_velocity: Object { x: 0.0, y: -39.0 },
+                time: 5.0,
+            })
+        );
+
+        assert_eq!(
+            obj.next(),
+            Some(ThrowObject {
+                init_position: Object { x: 0.0, y: 150.0 },
+                init_velocity: Object { x: 0.0, y: 10.0 },
+                actual_position: Object { x: 0.0, y: 33.6 },
+                actual_velocity: Object { x: 0.0, y: -48.8 },
+                time: 6.0,
+            })
+        );
+
+        assert!(obj.next().is_none(), "{:?} instead of None", obj);
     }
 }
