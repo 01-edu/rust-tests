57ca59d3ec6802ca77b6adedfc138f899d6f6c1a
davidrobert99 <davidedamata@gmail.com>
2022-04-21 12:00:08 +0100

    fix comments

--- solutions/error_types/src/lib.rs
+++ solutions/error_types/src/lib.rs
@@ -3,23 +3,23 @@
 
 ### Instructions
 
-For this exercise you will have to implement an **error type**.
+For this exercise, you will have to implement an **error type**.
 
-The main objective is to create a form validator, where you must implement a
+The main objective is to create a form validator, where you must implement an
 error type. This must validate the password and the first name. The
-first name must not be empty and the password must have at least 8 char and a combination of alphanumeric and none alphanumeric ASCII characters
+first name must not be empty and the password must have **at least 8 char**, a **combination of alphanumeric**, **none-alphanumeric** (ex: <, & or /, ...)
 
-ex: "asDd123=%" => good
-    "asgfD"     => error
-    "asdsdf2"   => error
-    "sad_#$"    => error
+ex: "asDd123=%" => good (as 8 char, alphanumeric and non-alphanumeric)\
+"asgfD" => error (does only have alphabetic letters)\
+"asdsdf2" => error (missing none-alphanumeric)\
+"sad\_#$" => error (does not have a combination of alphanumeric)
 
 Create a structure called `Form` that will have the following fields:
 
 - `first_name`, that will be a string
 - `last_name`, that will be a string
 - `birth`, of type `NaiveDate` that will convert a string "2015-09-05" to a date of that format
-- `sex`, SexType that must be a `enum` with the fields `Male` and `Female`
+- `fav_colour`, of type Color that must be a `enum` with the fields `Red`, `Blue`  and `Green`
 - `birth_location`, that will be a string
 - `password`, that will be a string
 
@@ -31,14 +31,14 @@ It must have the fields:
 
 - `form_values`, this will be a tuple of strings that will save the value that the user inserted into the form
 
-ex: ("password", "asdaSD_")
-    ("first_name", "someone")
+ex: ("password", "asdaSD\_")
+("first_name", "someone")
 
 - `date`, that will have the date that the error occurred in the format "2020-12-14 09:33:41"
 - `err`, that will have the error description:
-    - "No user name"
-    - "At least 8 characters"
-    - "Combination of different ASCII character types (numbers, letters and none alphanumeric characters)"
+  - "No user name"
+  - "At least 8 characters"
+  - "Combination of different ASCII character types (numbers, letters and none alphanumeric characters)"
 
 ### Example
 
@@ -48,38 +48,26 @@ fn main() {
         String::from("Lee"),
         String::from("Silva"),
         create_date("2015-09-05"),
-        SexType::Male,
+        Color::Red,
         String::from("Africa"),
-        String::from("qwqwsa1dty_"));
+        String::from("qwqwsa1dty_"),
+    );
 
     println!("{:?}", form_output);
-    // output:
-    // Form { first_name: "Lee", last_name: "Silva", birth: 2015-09-05, sex: Male), birth_location: "Africa"), password: "qwqwsa1dty_"}
     println!("{:?}", form_output.validate().unwrap());
-    // output:
-    // ["Valid password", "Valid first name"]
+
     form_output.first_name = String::from("");
     println!("{:?}", form_output.validate().unwrap_err());
-    // output:
-    // FErr { form_values: ("first_name", ""), date: "2020-12-11 18:37:08", err: "No user name" }
 
+    form_output.first_name = String::from("as");
     form_output.password = String::from("dty_1");
-
     println!("{:?}", form_output.validate().unwrap_err());
-    // output:
-    // FErr { form_values: ("password", "dty_1"), date: "2020-12-11 18:49:31", err: "At least 8 characters" }
 
     form_output.password = String::from("asdasASd(_");
-
     println!("{:?}", form_output.validate().unwrap_err());
-    // output:
-    // FErr { form_values: ("password", "asdasASd(_"), date: "2020-12-11 18:51:10", err: "Combination of different ASCII character types (numbers, letters and am none alphanumeric characters)" }
 
     form_output.password = String::from("asdasASd123SA");
-
     println!("{:?}", form_output.validate().unwrap_err());
-    // output:
-    // FErr { form_values: ("password", "asdasASd123SA", date: "2020-12-11 18:51:10", err: "Combination of different ASCII character types (numbers, letters and am none alphanumeric characters)" }
 }
 ```
 
