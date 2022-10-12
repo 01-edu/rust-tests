13aeb90878432980a9a5b4cef6ed53dd1c15eb48
Michele Sessa <mikysett@gmail.com>
2022-10-12 15:44:24 +0100

    refactor(tic_tac_toe): all results are now lowercase
    also remove subject from solution and tests

--- solutions/tic_tac_toe/src/lib.rs
+++ solutions/tic_tac_toe/src/lib.rs
@@ -1,57 +1,3 @@
-/*
-## tic_tac_toe
-
-### Instructions
-
-You must create a tic tac toe checker.
-
-Create the following functions:
-
-- `tic_tac_toe` that receives a table of vectors (Vec<Vec<&str>>) and returns a string : `player O won` or `player X won` or `Tie`
-- `diagonals` that will receive a player and a table. It should return a boolean, this must return true if all the diagonals are completed by the player
-- `horizontal` that will receive a player and a table. It should return a boolean, this must return true if one of the horizontal lines are completed by the player
-- `vertical` that will receive a player and a table. It should return a boolean, this must return true if one of the vertical lines are completed by the player
-
-### Example
-
-```rust
-fn main() {
-    println!(
-        "{:?}",
-        tic_tac_toe(vec![
-            vec!["O", "X", "O"],
-            vec!["O", "P", "X"],
-            vec!["X", "#", "X"]
-        ])
-    );
-    // "Tie"
-    println!(
-        "{:?}",
-        tic_tac_toe(vec![
-            vec!["X", "O", "O"],
-            vec!["X", "O", "O"],
-            vec!["#", "O", "X"]
-        ])
-    );
-    // "player O won"
-
-    let dig = vec![
-            vec!["O", "O", "X"],
-            vec!["O", "X", "O"],
-            vec!["X", "#", "X"]
-        ];
-
-    println!("{:?}",tic_tac_toe(dig));
-    // "player X won"
-}
-```
-
-### Notions
-
-- https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html
-
-*/
-
 pub fn check(player: &str, table: &Vec<Vec<&str>>) -> bool {
     diagonals(player, &table) || horizontal(player, &table) || vertical(player, &table)
 }
@@ -64,7 +10,7 @@ pub fn tic_tac_toe(table: Vec<Vec<&str>>) -> String {
     } else if check(player1, &table) {
         return format!("player {player1} won");
     } else {
-        return "Tie".to_string();
+        return "tie".to_string();
     }
 }
 
