192fc618454de0fd3800da1559bd10e16178ddb6
Michele Sessa <mikysett@gmail.com>
2022-10-12 11:24:17 +0100

    feat(simple_hash): rewrite exercise completely
    improve the way students are introduced to hash maps

--- solutions/simple_hash/src/lib.rs
+++ solutions/simple_hash/src/lib.rs
@@ -12,4 +12,3 @@ pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
     frequency_count.keys().len()
 }
-
