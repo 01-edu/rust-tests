012bf538b61c59981af7c33eba8fcbc61b9cfc26
Michele Sessa <mikysett@gmail.com>
2022-10-20 10:50:26 +0100

    refactor(error_types): remove unused function and useless comments

--- solutions/error_types/src/lib.rs
+++ solutions/error_types/src/lib.rs
@@ -1,8 +1,5 @@
 pub use chrono::{NaiveDate, Utc};
-// use chrono::format::ParseError;
-use std::fmt;
 
-// this will be the structure that wil handle the errors
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
     pub form_values: (String, String),
@@ -17,16 +14,6 @@ impl FormError {
             date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
             err,
         }
-    }
-}
-
-impl fmt::Display for FormError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} - form values: {:?}, err: {}",
-            &self.date, &self.form_values, &self.err
-        )
     }
 }
 
@@ -56,8 +43,6 @@ impl Form {
         }
     }
 
-    // this will validate the form, it does not validate if
-    // the requirements are not fulfilled
     pub fn validate(&self) -> Result<Vec<&str>, FormError> {
         let v: Vec<&str> = vec![
             match &self.first_name {
