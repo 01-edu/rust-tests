36d4ec68b928f2a76badff04d98db4d4ca85eb15
mikysett <mikysett@gmail.com>
2022-10-20 15:59:31 +0100

    feat(borrow_me_the_reference): remove need for eval

--- solutions/borrow_me_the_reference/src/lib.rs
+++ solutions/borrow_me_the_reference/src/lib.rs
@@ -1,45 +1,32 @@
-use meval::eval_str;
-
-fn d_a_b(s: String) -> String {
-    let mut new_string = String::new();
-    let mut count = 0;
-    for v in s.chars() {
-        if count != 0 && v != '+' {
-            count -= 1;
-            continue;
-        }
-        if v == '+' {
-            count += 1;
-            continue;
-        }
-        new_string.push(v);
-        if v == '-' {
-            new_string.pop();
-            new_string.pop();
-        }
-    }
-    new_string
-}
-// - If a value does **not implement Copy**, it must be **borrowed** and so will be passed by **reference**.
-// this is not good in rust because strings are not to be manipulated like this
-// but its a good view for the students to see how memory works in rust
 pub fn delete_and_backspace(s: &mut String) {
-    let a = d_a_b(s.clone());
+    let s_copy = s.clone();
     s.clear();
-    s.push_str(&a);
-}
 
-// - If a value does **not implement Copy**, it must be **borrowed** and so will be passed by **reference**.
-pub fn is_correct(v: &mut Vec<&str>) -> usize {
-    let mut percentage = 0;
-    for (i, equation) in v.clone().iter().enumerate() {
-        let a: Vec<&str> = equation.split('=').collect();
-        if eval_str(a[0]).unwrap() == a[1].parse::<f64>().unwrap() {
-            v[i] = "✔";
-            percentage += 1;
+    let mut skip_next = 0;
+    for v in s_copy.chars() {
+        if v == '-' {
+            s.pop();
+        } else if v == '+' {
+            skip_next += 1;
+        } else if skip_next > 0 {
+            skip_next -= 1;
         } else {
-            v[i] = "✘";
+            s.push(v);
         }
     }
-    (percentage * 100) / v.len()
+}
+
+pub fn do_operations(v: &mut Vec<String>) {
+    for (i, equation) in v.clone().iter().enumerate() {
+        let numbers: Vec<&str> = equation.split(|c| c == '+' || c == '-').collect();
+        let left_number = numbers[0].parse::<i32>().unwrap();
+        let right_number = numbers[1].parse::<i32>().unwrap();
+
+        let result = if equation.contains('+') {
+            left_number + right_number
+        } else {
+            left_number - right_number
+        };
+        v[i] = result.to_string();
+    }
 }
--- tests/borrow_me_the_reference_test/src/main.rs
+++ tests/borrow_me_the_reference_test/src/main.rs
@@ -1,33 +1,35 @@
 use borrow_me_the_reference::*;
 
-fn main() {
-}
+fn main() {}
 
 #[test]
 fn test_delete_and_backspace() {
     let mut a_1 = String::from("bpp--o+er+++sskroi-++lcw");
     let mut a_2 = String::from("hs-+deasdasd------l+++dsdp");
     let mut a_3 = String::from("pad-rtic+eulqw--+rar");
+    let mut a_4 = String::from("--++++");
+
     delete_and_backspace(&mut a_1);
     delete_and_backspace(&mut a_2);
     delete_and_backspace(&mut a_3);
+    delete_and_backspace(&mut a_4);
+
     assert_eq!(a_1, "borrow".to_string());
     assert_eq!(a_2, "help".to_string());
     assert_eq!(a_3, "particular".to_string());
+    assert_eq!(a_4, "".to_string());
 }
 #[test]
-fn test_is_correct() {
-    let mut b_1: Vec<&str> = vec!["2+2=4", "3+2=5", "10-3=3", "5+5=10"];
-    let mut b_2: Vec<&str> = vec!["1+2=4", "0+2=5", "10-3=3", "41+5=10"];
-    let mut b_3: Vec<&str> = vec!["2+2=4", "3+2=5", "10-3=7", "5+5=10"];
-    let result_1 = is_correct(&mut b_1);
-    let result_2 = is_correct(&mut b_2);
-    let result_3 = is_correct(&mut b_3);
-    assert_eq!(result_1, 75);
-    assert_eq!(result_2, 0);
-    assert_eq!(result_3, 100);
+fn test_do_operations() {
+    let mut b_1: Vec<String> = vec![
+        "2+2".to_string(),
+        "3+2".to_string(),
+        "10-3".to_string(),
+        "0+0".to_string(),
+        "0-0".to_string(),
+        "10-100".to_string(),
+    ];
+    do_operations(&mut b_1);
 
-    assert_eq!(b_1, vec!["✔", "✔", "✘", "✔"]);
-    assert_eq!(b_2, vec!["✘", "✘", "✘", "✘"]);
-    assert_eq!(b_3, vec!["✔", "✔", "✔", "✔"]);
+    assert_eq!(b_1, vec!["4", "5", "7", "0", "0", "-90"]);
 }
