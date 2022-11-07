b11113819a153ccc323199a47b6b206739bdc315
eslopfer <lopezesteban8@gmail.com>
2022-11-07 19:09:22 +0000

    test(modify_letter): add tests for exam exercise

---
+++ tests/modify_letter_test/src/main.rs
@@ -0,0 +1,34 @@
+use modify_letter::*;
+
+fn main() {
+    println!("{}", remove_letter_sensitive("hEey hEey", 'e'));
+    println!("{}", remove_letter_insensitive("hEye", 'e'));
+    println!("{}", swap_letter_case("BbBb", 'b'));
+    println!("{}", remove_letter_sensitive("", 'a'));
+    println!("{}", remove_letter_sensitive("a", '\0'));
+}
+
+#[test]
+fn test_remove_letter_sensitive() {
+    assert_eq!(remove_letter_sensitive("Joje jis mijssjing", 'j'), "Joe is missing")
+}
+
+#[test]
+fn test_remove_letter_insensitive() {
+    assert_eq!(remove_letter_insensitive("JaillA ais swiaAmmingA", 'A'), "Jill is swimming")
+}
+
+#[test]
+fn test_swap_letter_case() {
+    assert_eq!(swap_letter_case("hEllo therE", 'e'), "hello thEre")
+}
+
+#[test]
+fn test_empty_arguments() {
+    assert_eq!(remove_letter_sensitive("", '\0'), "");
+    assert_eq!(remove_letter_insensitive("", '\0'), "");
+    assert_eq!(swap_letter_case("", '\0'), "");
+    assert_eq!(remove_letter_sensitive("a", '\0'), "a");
+    assert_eq!(remove_letter_insensitive("a", '\0'), "a");
+    assert_eq!(swap_letter_case("a", '\0'), "a");
+}
