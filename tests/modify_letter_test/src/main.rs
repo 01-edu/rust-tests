a2ec5f12dd9976f1479f2eb31efe384a52f61b96
eslopfer <lopezesteban8@gmail.com>
2022-11-09 19:02:29 +0000

    refactor(modify_letter): wrap tests into mod

--- tests/modify_letter_test/src/main.rs
+++ tests/modify_letter_test/src/main.rs
@@ -8,27 +8,31 @@ fn main() {
     println!("{}", remove_letter_sensitive("a", '\0'));
 }
 
-#[test]
-fn test_remove_letter_sensitive() {
-    assert_eq!(remove_letter_sensitive("Joje jis mijssjing", 'j'), "Joe is missing")
-}
-
-#[test]
-fn test_remove_letter_insensitive() {
-    assert_eq!(remove_letter_insensitive("JaillA ais swiaAmmingA", 'A'), "Jill is swimming")
-}
-
-#[test]
-fn test_swap_letter_case() {
-    assert_eq!(swap_letter_case("hEllo therE", 'e'), "hello thEre")
-}
-
-#[test]
-fn test_empty_arguments() {
-    assert_eq!(remove_letter_sensitive("", '\0'), "");
-    assert_eq!(remove_letter_insensitive("", '\0'), "");
-    assert_eq!(swap_letter_case("", '\0'), "");
-    assert_eq!(remove_letter_sensitive("a", '\0'), "a");
-    assert_eq!(remove_letter_insensitive("a", '\0'), "a");
-    assert_eq!(swap_letter_case("a", '\0'), "a");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_remove_letter_sensitive() {
+        assert_eq!(remove_letter_sensitive("Joje jis mijssjing", 'j'), "Joe is missing")
+    }
+    
+    #[test]
+    fn test_remove_letter_insensitive() {
+        assert_eq!(remove_letter_insensitive("JaillA ais swiaAmmingA", 'A'), "Jill is swimming")
+    }
+    
+    #[test]
+    fn test_swap_letter_case() {
+        assert_eq!(swap_letter_case("hEllo therE", 'e'), "hello thEre")
+    }
+    
+    #[test]
+    fn test_empty_arguments() {
+        assert_eq!(remove_letter_sensitive("", '\0'), "");
+        assert_eq!(remove_letter_insensitive("", '\0'), "");
+        assert_eq!(swap_letter_case("", '\0'), "");
+        assert_eq!(remove_letter_sensitive("a", '\0'), "a");
+        assert_eq!(remove_letter_insensitive("a", '\0'), "a");
+        assert_eq!(swap_letter_case("a", '\0'), "a");
+    }
 }
