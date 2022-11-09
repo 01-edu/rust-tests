b18f82894e999b7a8531e74c1ae93961ff1913a2
eslopfer <lopezesteban8@gmail.com>
2022-11-09 18:49:55 +0000

    refactor(modify_letter): remove unnecessary matches

--- solutions/modify_letter/src/lib.rs
+++ solutions/modify_letter/src/lib.rs
@@ -1,51 +1,34 @@
 pub fn remove_letter_sensitive(s: &str, letter: char) -> String {
-    match s.len() {
-        0 => s.to_string(),
-        _ => match letter {
-            '\0' => s.to_string(),
-            _ => s.replace(letter, "")
-        }
-    }
+    s.replace(letter, "")
 }
 
 pub fn remove_letter_insensitive(s: &str, letter: char) -> String {
-    match s.len() {
-        0 => s.to_string(),
-        _ => match letter {
-            '\0' => s.to_string(),
-            _ => {
-                let lower_letter = letter.to_lowercase().to_string();
-                let upper_letter = letter.to_uppercase().to_string();
-                s
-                    .replace(&upper_letter, &lower_letter)
-                    .replace(&lower_letter, "")
-            }
-        }
-    }
+    let lower_letter = letter.to_lowercase().to_string();
+    let upper_letter = letter.to_uppercase().to_string();
+    s
+        .replace(&upper_letter, &lower_letter)
+        .replace(&lower_letter, "")
     
 }
 
 pub fn swap_letter_case(s: &str, letter: char) -> String {
-    match s.len() {
-        0 => s.to_string(),
-        _ => s.split("")
-            .into_iter()
-            .map(|s| {
-                s.chars()
-                    .filter_map(|c| {
-                        if c.to_string() == letter.to_lowercase().to_string() {
-                            c.to_uppercase().next()
-                        } else if c.to_string() == letter.to_uppercase().to_string() {
-                            c.to_lowercase().next()
-                        } else {
-                            Some(c)
-                        }
-                    })
-                    .collect()
-            })
-            .collect::<Vec<String>>()
-            .join(""),
-    }
+    s.split("")
+        .into_iter()
+        .map(|s| {
+            s.chars()
+                .filter_map(|c| {
+                    if c.to_string() == letter.to_lowercase().to_string() {
+                        c.to_uppercase().next()
+                    } else if c.to_string() == letter.to_uppercase().to_string() {
+                        c.to_lowercase().next()
+                    } else {
+                        Some(c)
+                    }
+                })
+                .collect()
+        })
+        .collect::<Vec<String>>()
+        .join("")
 }
 
 #[cfg(test)]
