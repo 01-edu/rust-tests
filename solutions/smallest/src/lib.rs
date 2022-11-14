9bb8e5ee6099e7e9689a04065bd8ea1c54b712fd
eslopfer <lopezesteban8@gmail.com>
2022-11-14 18:47:17 +0000

    feat(smallest): remove positive constraint

--- solutions/smallest/src/lib.rs
+++ solutions/smallest/src/lib.rs
@@ -4,7 +4,7 @@ pub fn smallest(h: HashMap<&str, i32>) -> i32 {
     let mut smallest_value = i32::MAX;
 
     for (_, v) in h.iter() {
-        if *v >= 0 && *v < smallest_value {
+        if *v < smallest_value {
             smallest_value = *v;
         }
     }
