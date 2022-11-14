ea101384ff90ade94e977137eb84702045f4c22e
nprimo <primo.niccolo@gmail.com>
2022-11-14 12:07:03 +0000

    refactor(min_and_max): remove tests from solution

--- solutions/min_and_max/src/lib.rs
+++ solutions/min_and_max/src/lib.rs
@@ -17,16 +17,3 @@ pub fn min_and_max(nb_1: i32, nb_2: i32, nb_3: i32) -> (i32, i32) {
 
     (tmp_min, tmp_max)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(min_and_max(-1, 2, 4), (-1, 4));
-        assert_eq!(min_and_max(3, 2, 1), (1, 3));
-        assert_eq!(min_and_max(-23, -43, 0), (-43, 0));
-        assert_eq!(min_and_max(15, 15, 15), (15, 15));
-    }
-}
