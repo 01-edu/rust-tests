43a8b40740d302906546e4cf065cfbe335ea524d
eslopfer <lopezesteban8@gmail.com>
2022-11-16 18:44:27 +0000

    test(check_user_name): add missing tests for send name function

--- tests/check_user_name_test/src/main.rs
+++ tests/check_user_name_test/src/main.rs
@@ -17,8 +17,11 @@ mod tests {
 
     #[test]
     fn test_error() {
-        let u = User::new("Michael".to_string(), AccessLevel::Guest);
-        assert_eq!(check_user_name(&u), (false, "ERROR: User is guest"));
+        let guest_case = User::new("Michael".to_string(), AccessLevel::Guest);
+        assert_eq!(
+            check_user_name(&guest_case),
+            (false, "ERROR: User is guest")
+        );
     }
 
     #[test]
@@ -27,5 +30,15 @@ mod tests {
         let normal_case = User::new("Sara".to_string(), AccessLevel::Normal);
         assert_eq!(check_user_name(&admin_case), (true, "Fatima"));
         assert_eq!(check_user_name(&normal_case), (true, "Sara"));
+    }
+
+    #[test]
+    fn test_send_name() {
+        let admin_case = User::new("Fatima".to_string(), AccessLevel::Admin);
+        let normal_case = User::new("Sara".to_string(), AccessLevel::Normal);
+        let guest_case = User::new("Michael".to_string(), AccessLevel::Guest);
+        assert_eq!(admin_case.send_name(), Some("Fatima"));
+        assert_eq!(normal_case.send_name(), Some("Sara"));
+        assert_eq!(guest_case.send_name(), None);
     }
 }
