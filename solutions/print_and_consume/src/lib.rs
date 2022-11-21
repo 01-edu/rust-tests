d83814d7a4b25fd47740d2139ad711154081546f
eslopfer <lopezesteban8@gmail.com>
2022-11-21 19:06:11 +0000

    feat(print_and_consume): add partial solution and tests

--- solutions/print_and_consume/src/lib.rs
+++ solutions/print_and_consume/src/lib.rs
@@ -1,15 +1,26 @@
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        print_and_consume("Hello".to_string())
+    #[should_panic]
+    fn test_consume() {
+        let to_consume = "Hello".to_string();
+        print_and_consume(to_consume);
+        println!("{}", to_consume);
+    }
+    #[test]
+    fn test_only_print() {
+        let only_to_print = "Hello".to_string();
+        only_print(&only_to_print);
+        println!("{}", only_to_print);
     }
 }
 
 pub fn print_and_consume(to_consume: String) {
     println!("{}", to_consume);
+}
+
+pub fn only_print(only_to_print: &String) {
+    println!("{}", only_to_print)
 }
