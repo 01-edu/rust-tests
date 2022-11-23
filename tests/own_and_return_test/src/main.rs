627df0a770d8b1f7b7ce4ccb08c26c6d32a31c06
eslopfer <lopezesteban8@gmail.com>
2022-11-23 15:55:55 +0000

    refactor(own_and_return_test): refactor tests to be more robust

--- tests/own_and_return_test/src/main.rs
+++ tests/own_and_return_test/src/main.rs
@@ -1,27 +1,26 @@
 use own_and_return::*;
 
-fn main() {
-}
+fn main() {}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_consume() {
-        println!(
-            "{}",
+        assert_eq!(
             take_film_name(Film {
                 name: "Matrix".to_string()
-            })
+            }),
+            "Matrix".to_string()
         );
     }
     #[test]
     fn test_only_print() {
-        println!(
-            "{}",
+        assert_eq!(
             read_film_name(&Film {
                 name: "Matrix".to_string()
-            })
-        );
+            }),
+            "Matrix".to_string()
+        )
     }
 }
