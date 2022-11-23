f42869ee0145addce99218de674d7f9899e72ea6
eslopfer <lopezesteban8@gmail.com>
2022-11-23 12:06:49 +0000

    feat(print_and_consume): add solution and tests for exercise

--- solutions/print_and_consume/src/lib.rs
+++ solutions/print_and_consume/src/lib.rs
@@ -1,26 +1,11 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[should_panic]
-    fn test_consume() {
-        let to_consume = "Hello".to_string();
-        print_and_consume(to_consume);
-        println!("{}", to_consume);
-    }
-    #[test]
-    fn test_only_print() {
-        let only_to_print = "Hello".to_string();
-        only_print(&only_to_print);
-        println!("{}", only_to_print);
-    }
+pub struct Film {
+    pub name: String,
 }
 
-pub fn print_and_consume(to_consume: String) {
-    println!("{}", to_consume);
+pub fn own_and_return(to_consume: Film) -> String {
+    to_consume.name
 }
 
-pub fn only_print(only_to_print: &String) {
-    println!("{}", only_to_print)
+pub fn only_return(only_to_return: &Film) -> String {
+    only_to_return.name.clone()
 }
--- tests/print_and_consume_test/src/main.rs
+++ tests/print_and_consume_test/src/main.rs
@@ -1,3 +1,39 @@
+use print_and_consume::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{}",
+        own_and_return(Film {
+            name: "Casablanca".to_string()
+        })
+    );
+    println!(
+        "{}",
+        only_return(&Film {
+            name: "Casablanca".to_string()
+        })
+    );
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_consume() {
+        println!(
+            "{}",
+            own_and_return(Film {
+                name: "Matrix".to_string()
+            })
+        );
+    }
+    #[test]
+    fn test_only_print() {
+        println!(
+            "{}",
+            only_return(&Film {
+                name: "Matrix".to_string()
+            })
+        );
+    }
 }
