441aedbcb8596e1e2bd92c4d7041c14ffe849042
eslopfer <lopezesteban8@gmail.com>
2022-11-24 19:20:02 +0000

    test(own_and_return_test): add usage example

--- tests/own_and_return_test/src/main.rs
+++ tests/own_and_return_test/src/main.rs
@@ -1,6 +1,15 @@
 use own_and_return::*;
 
-fn main() {}
+fn main() {
+    let my_film = Film {
+        name: "Terminator".to_string(),
+    };
+    // println!("{}", take_film_name(my_film));
+    // uncomment the previous line to check that the value is consumed
+    println!("{}", read_film_name(&my_film));
+    println!("{}", take_film_name(my_film));
+    // you can test this function by commenting out the first print statement, you should see the expected output without errors in this case
+}
 #[cfg(test)]
 mod tests {
     use super::*;
