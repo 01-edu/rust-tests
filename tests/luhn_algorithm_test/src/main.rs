ed822d3f4b22732898e91b9b61d93fce931b55a9
Michele Sessa <mikysett@gmail.com>
2023-09-21 12:59:47 +0100

    tests(luhn_algorithm): add more examples in fn main

--- tests/luhn_algorithm_test/src/main.rs
+++ tests/luhn_algorithm_test/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     println!("{}", is_luhn_formula(""));
     println!("{}", is_luhn_formula("1"));
     println!("{}", is_luhn_formula("79927398713"));
+    println!("{}", is_luhn_formula("7992 7398 713"));
+    println!("{}", is_luhn_formula("1234567890123456"));
 }
 
 #[cfg(test)]
