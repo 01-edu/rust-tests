9b5eb8aeb9be98624ce55dd12c74b756d32023ed
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 10:41:40 +0000

    fix(fibonacci2): changed exercise

--- solutions/fibonacci2/src/lib.rs
+++ solutions/fibonacci2/src/lib.rs
@@ -1,24 +1,7 @@
-// fn main() {
-// 	let val: u32 = 20;
-// 	println!("Fibonacci({}) = {}", val, fibonacci(val));
-//}
-
 pub fn fibonacci(n: u32) -> u32 {
-    if n < 2 {
-        return n;
-    }
-    fibonacci(n - 2) + fibonacci(n - 1)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(fibonacci(0), 0);
-        assert_eq!(fibonacci(1), 1);
-        assert_eq!(fibonacci(22), 17711);
-        assert_eq!(fibonacci(20), 6765);
+    if matches!(n, 0 | 1) {
+        n
+    } else {
+        fibonacci(n - 2) + fibonacci(n - 1)
     }
 }
