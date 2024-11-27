fdb8ee079073cba74137cf69cb19badcdd403209
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 11:20:04 +0000

    fix(reverse_string)

--- tests/reverse_string_test/src/main.rs
+++ tests/reverse_string_test/src/main.rs
@@ -10,29 +10,31 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use reverse_string::*;
+
     #[test]
     fn test_one_word() {
-        assert_eq!(rev_str("robot", "tobor"));
-        assert_eq!(rev_str("Ramen", "nemaR"));
-        assert_eq!(rev_str("racecar", "racecar"));
-        assert_eq!(rev_str("drawer", "reward"));
-        assert_eq!(rev_str("子猫", "猫子"));
-        assert_eq!(rev_str("", ""));
+        assert_eq!(rev_str("robot"), "tobor");
+        assert_eq!(rev_str("Ramen"), "nemaR");
+        assert_eq!(rev_str("racecar"), "racecar");
+        assert_eq!(rev_str("drawer"), "reward");
+        assert_eq!(rev_str("子猫"), "猫子");
+        assert_eq!(rev_str(""), "");
     }
 
     #[test]
     fn test_multiple_words() {
-        assert_eq!(rev_str("I'm hungry!", "!yrgnuh m'I"));
-        assert_eq!(rev_str("Hello, world!", "!dlrow ,olleH"));
-        assert_eq!(rev_str(
-            "Hello, my name is Roman",
+        assert_eq!(rev_str("I'm hungry!"), "!yrgnuh m'I");
+        assert_eq!(rev_str("Hello, world!"), "!dlrow ,olleH");
+        assert_eq!(
+            rev_str("Hello, my name is Roman"),
             "namoR si eman ym ,olleH"
-        ));
-        assert_eq!(rev_str("I have a nice car!", "!rac ecin a evah I"));
-        assert_eq!(rev_str("How old are You", "uoY era dlo woH"));
-        assert_eq!(rev_str(
-            "ex: this is an example água",
+        );
+        assert_eq!(rev_str("I have a nice car!"), "!rac ecin a evah I");
+        assert_eq!(rev_str("How old are You"), "uoY era dlo woH");
+        assert_eq!(
+            rev_str("ex: this is an example água"),
             "augá elpmaxe na si siht :xe"
-        ));
+        );
     }
 }
