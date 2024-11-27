dc287b462797f6c534a0b30ca1aa7cdbc19ad60e
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 10:56:00 +0000

    fix(tuples_refs): added tests module

--- tests/tuples_refs_test/src/main.rs
+++ tests/tuples_refs_test/src/main.rs
@@ -7,20 +7,25 @@ fn main() {
     println!("Student's id: {}", id(&student));
 }
 
-#[test]
-fn test_id() {
-    let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-    assert_eq!(id(&student), 20);
-}
+#[cfg(test)]
+mod tests {
+    use tuples_refs::*;
 
-#[test]
-fn test_first_name() {
-    let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-    assert_eq!(first_name(&student), "Pedro");
-}
+    #[test]
+    fn test_id() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(id(&student), 20);
+    }
 
-#[test]
-fn test_last_name() {
-    let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-    assert_eq!(last_name(&student), "Domingos");
+    #[test]
+    fn test_first_name() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(first_name(&student), "Pedro");
+    }
+
+    #[test]
+    fn test_last_name() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(last_name(&student), "Domingos");
+    }
 }
