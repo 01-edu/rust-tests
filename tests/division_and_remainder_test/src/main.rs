a0afbfdde18cc6eaabd7b82084bb2d87780199e6
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 10:53:32 +0000

    fix(division_and_remainder): changed exercise

--- tests/division_and_remainder_test/src/main.rs
+++ tests/division_and_remainder_test/src/main.rs
@@ -5,31 +5,31 @@ fn main() {
     let y = 4;
     let (division, remainder) = divide(x, y);
     println!(
-        "\t{}/{}: division = {}, remainder = {}",
+        "{}/{}: division = {}, remainder = {}",
         x, y, division, remainder
     );
 }
 
-#[test]
-#[should_panic]
-fn divide_by_0() {
-    divide(40, 0);
-}
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[should_panic]
+    fn test_divide_by_0() {
+        divide(40, 0);
+    }
 
-#[test]
-fn test_divide() {
-    let (div, rem) = divide(40, 3);
+    #[test]
+    fn test_divide() {
+        let (div, rem) = divide(40, 3);
+        assert_eq!(div, 13);
+        assert_eq!(rem, 1);
 
-    assert_eq!(div, 13);
-    assert_eq!(rem, 1);
+        let (div, rem) = divide(389, 39);
+        assert_eq!(div, 9);
+        assert_eq!(rem, 38);
 
-    let (div, rem) = divide(389, 39);
-
-    assert_eq!(div, 9);
-    assert_eq!(rem, 38);
-
-    let (div, rem) = divide(29, 332);
-
-    assert_eq!(div, 0);
-    assert_eq!(rem, 29);
+        let (div, rem) = divide(29, 332);
+        assert_eq!(div, 0);
+        assert_eq!(rem, 29);
+    }
 }
