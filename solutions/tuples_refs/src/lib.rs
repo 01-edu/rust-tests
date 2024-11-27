9e3d1c96e6550b836a0bb1875eababdb97299078
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 00:44:45 +0000

    fix(tuples_refs): changed exercise

--- solutions/tuples_refs/src/lib.rs
+++ solutions/tuples_refs/src/lib.rs
@@ -1,52 +1,13 @@
-// Define a tuple to represent a student
-// Each student is identified by an id number of type u32
-// Its name and its last name as a string
-// Print the content of the tuple to stdout
-
-#[derive(Debug)]
 pub struct Student(pub u32, pub String, pub String);
 
 pub fn id(student: &Student) -> u32 {
     student.0
 }
 
-pub fn first_name(student: &Student) -> String {
-    student.1.clone()
+pub fn first_name(student: &Student) -> &str {
+    &student.1
 }
 
-pub fn last_name(student: &Student) -> String {
-    student.2.clone()
-}
-// fn main() {
-// 	let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-// 	println!("Student: {:?}", student);
-// 	// After print only the first name
-// 	println!("Student first name: {}", student.1);
-// 	// After print only the last name
-// 	println!("Student last name: {}", student.2);
-// 	// After print only the id
-// 	println!("Student Id: {}", student.0);
-// }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_id() {
-        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-        assert_eq!(id(&student), 20);
-    }
-
-    #[test]
-    fn test_first_name() {
-        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-        assert_eq!(first_name(&student), "Pedro".to_string());
-    }
-
-    #[test]
-    fn test_last_name() {
-        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-        assert_eq!(last_name(&student), "Domingos".to_string());
-    }
+pub fn last_name(student: &Student) -> &str {
+    &student.2
 }
--- tests/tuples_refs_test/src/main.rs
+++ tests/tuples_refs_test/src/main.rs
@@ -1,19 +1,10 @@
-// Define a tuple to represent a student
-// Each student is identified by an id number of type i32
-// Its name and its last name as a string
-// Print the content of the tuple to stdout
-
 use tuples_refs::*;
 
 fn main() {
     let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-    println!("Student: {:?}", student);
-    // After print only the first name
-    println!("Student first name: {}", first_name(&student));
-    // After print only the last name
-    println!("Student last name: {}", last_name(&student));
-    // After print only the id
-    println!("Student Id: {}", id(&student));
+    println!("Student's first name: {}", first_name(&student));
+    println!("Student's last name: {}", last_name(&student));
+    println!("Student's id: {}", id(&student));
 }
 
 #[test]
@@ -25,11 +16,11 @@ fn test_id() {
 #[test]
 fn test_first_name() {
     let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-    assert_eq!(first_name(&student), "Pedro".to_string());
+    assert_eq!(first_name(&student), "Pedro");
 }
 
 #[test]
 fn test_last_name() {
     let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-    assert_eq!(last_name(&student), "Domingos".to_string());
+    assert_eq!(last_name(&student), "Domingos");
 }
