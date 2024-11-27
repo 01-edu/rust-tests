0e47ed6dae57e5cd36f7ebafc7b5df0d333f7dbf
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-11-27 10:49:11 +0000

    fix(matrix_transposition): changed exercise

--- solutions/matrix_transposition/src/lib.rs
+++ solutions/matrix_transposition/src/lib.rs
@@ -1,18 +1,3 @@
-// Define a function that calculate the transpose matrix of a 2x2 matrix
-// You don't need to understand everything about matrices
-
-// Just convert lines into columns and vice versa
-// ( a b )	__ transposition __> ( a d )
-// ( c d )  					 ( b d )
-
-// Only the body of the transpose function can be changed
-
-// fn main() {
-// 	let matrix = Matrix((1, 3), (4, 5));
-// 	println!("Original matrix {:?}", matrix);
-// 	println!("Transpose matrix {:?}", transpose(matrix));
-// }
-
 #[derive(Debug, PartialEq, Eq)]
 pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
