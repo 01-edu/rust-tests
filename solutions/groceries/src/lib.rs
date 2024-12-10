83d9d7abf38fc6c63dc0769f235b7a33bee69f1e
Pedro Ferreira <me.pedro.ferreira@gmail.com>
2024-12-10 00:26:35 +0000

    fix(rust-quest01): applied proposed changes

--- solutions/groceries/src/lib.rs
+++ solutions/groceries/src/lib.rs
@@ -1,37 +1,7 @@
-// # Instructions
-
-// Create a function called `insert`
-// fn insert(vec: &mut Vec<String>, val: String) that inserts a new element at the end of the Vec
-
 pub fn insert(vec: &mut Vec<String>, val: String) {
     vec.push(val);
 }
 
-pub fn at_index(vec: &Vec<String>, index: usize) -> String {
-    vec[index].clone()
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_insertions() {
-        let mut groceries = Vec::new();
-        insert(&mut groceries, "milk".to_string());
-        assert_eq!(groceries, ["milk"]);
-        insert(&mut groceries, "bread".to_string());
-        assert_eq!(groceries, ["milk", "bread"]);
-    }
-
-    #[test]
-    fn test_index() {
-        let groceries: Vec<String> = vec![
-            "milk".to_string(),
-            "bread".to_string(),
-            "water".to_string(),
-            "wine".to_string(),
-        ];
-        assert_eq!(at_index(&groceries, 0), "milk");
-    }
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    &slice[index]
 }
--- solutions/looping/src/main.rs
+++ solutions/looping/src/main.rs
@@ -1,41 +1,26 @@
-// Write a program that prints a riddle, receives input from the user
-// and checks that the answer is correct
-
-// The program must allow indefinite number of trials and only quit after the
-// correct answer is given
-
-// Every time the user introduces an incorrect answer the program must
-// print the riddle
-// Before quitting the program must print the number of trials
-
-// Riddle: I am the beginning of the end, and the end of time and
-// space. I am essential to creation, and I surround every place. What
-// am I?
-
-// Answer: The letter e
-
 use std::io;
 
-fn main() {
-    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
-    let answer = "The letter e";
-    let mut counter = 0;
+const RIDDLE: &str = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+const ANSWER: &str = "The letter e\n";
 
-    let trials = loop {
-        println!("{}", riddle);
+fn main() {
+    let mut trials = 0;
+
+    loop {
+        println!("{}", RIDDLE);
 
         let mut input = String::new();
 
         io::stdin()
             .read_line(&mut input)
-            .expect("Failed to read line");
+            .expect("Couldn't read line");
 
-        counter += 1;
+        trials += 1;
 
-        if answer.to_owned() + "\n" == input {
-            break counter;
+        if input == ANSWER {
+            break;
         }
-    };
+    }
 
-    println!("Number of trials: {}", trials);
+    println!("Number of trials: {trials}");
 }
