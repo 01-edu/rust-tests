3a741d5ded5088384c11fc2a0d87aa29eb6a82d6
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-12-11 12:55:54 +0000

    fix(armstrong_number): changed exercise

--- solutions/armstrong_number/src/lib.rs
+++ solutions/armstrong_number/src/lib.rs
@@ -1,26 +1,15 @@
 pub fn is_armstrong_number(nb: u32) -> Option<u32> {
-    let nb_digits = nb_digits(nb);
-    let mut new_num = nb;
-    let mut armstrong: u32 = 0;
+    let digits = nb.checked_ilog10().unwrap_or(0) + 1;
 
-    while new_num != 0 {
-        let digit = new_num % 10;
-        match armstrong.checked_add(digit.pow(nb_digits)) {
-            Some(nb) => armstrong = nb,
-            None => return None,
-        }
+    let armstrong = (0..digits)
+        .scan(nb, |state, _| {
+            let val = (*state % 10).pow(digits);
 
-        new_num /= 10;
-    }
+            *state /= 10;
+
+            Some(val)
+        })
+        .sum::<u32>();
+
     (armstrong == nb).then_some(nb)
-}
-
-fn nb_digits(mut nb: u32) -> u32 {
-    let mut nb_digits = 0;
-
-    while nb != 0 {
-        nb_digits += 1;
-        nb /= 10;
-    }
-    nb_digits
 }
