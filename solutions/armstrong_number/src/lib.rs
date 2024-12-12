3fce2ff06bb088d2d72b27eccdc5f8aa7c2068ae
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-12-12 11:03:29 +0000

    fix(armstrong_number): minor change

--- solutions/armstrong_number/src/lib.rs
+++ solutions/armstrong_number/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn is_armstrong_number(nb: u32) -> Option<u32> {
-    let digits = nb.checked_ilog10().unwrap_or(0) + 1;
+    let digits = nb.checked_ilog10().unwrap_or_default() + 1;
 
     let armstrong = (0..digits)
         .scan(nb, |state, _| {
