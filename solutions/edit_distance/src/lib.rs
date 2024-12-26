57fe9b9d3635eb118c61cc1b936d968b24529d2f
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-12-26 00:58:05 +0000

    fix(edit_distance)

--- solutions/edit_distance/src/lib.rs
+++ solutions/edit_distance/src/lib.rs
@@ -1,67 +1,35 @@
-// Create a function call `edit_distance` that calculates the minimum
-// number of changes (insertion, deletions and substitutions) that
-// need to be made to a string `source` to arrive to another `target`
-// string
+// Levenshtein's distance using two matrix rows
 
-// For more information and examples https://en.wikipedia.org/wiki/Edit_distance
+use std::mem;
 
 pub fn edit_distance(source: &str, target: &str) -> usize {
-    let src = source.chars().collect::<Vec<_>>();
-    let tar = target.chars().collect::<Vec<_>>();
-    let source_len = src.len() + 1;
-    let target_len = tar.len() + 1;
+    let m = source.len();
+    let n = target.len();
 
-    if source_len == 0 {
-        return target_len;
-    }
-    if target_len == 0 {
-        return source_len;
-    }
+    let mut v0 = (0..=n).collect::<Vec<_>>();
+    let mut v1 = vec![0; n + 1];
 
-    let mut matrix = vec![vec![0; source_len]; target_len];
+    for i in 0..m {
+        v1[0] = i + 1;
 
-    for i in 1..target_len {
-        matrix[i][0] = i
-    }
-    for j in 1..source_len {
-        matrix[0][j] = j
-    }
+        for j in 0..n {
+            let deletion_cost = v0[j + 1] + 1;
+            let insertion_cost = v1[j] + 1;
+            let substitution_cost = v0[j]
+                + if source.chars().nth(i) == target.chars().nth(j) {
+                    0
+                } else {
+                    1
+                };
 
-    for i in 1..target_len {
-        for j in 1..source_len {
-            let x = if src[j - 1] == tar[i - 1] {
-                matrix[i - 1][j - 1]
-            } else {
-                1 + std::cmp::min(
-                    std::cmp::min(matrix[i][j - 1], matrix[i - 1][j]),
-                    matrix[i - 1][j - 1],
-                )
-            };
-            matrix[i][j] = x;
+            v1[j + 1] = [deletion_cost, insertion_cost, substitution_cost]
+                .into_iter()
+                .min()
+                .unwrap();
         }
+
+        mem::swap(&mut v0, &mut v1);
     }
-    matrix[target_len - 1][source_len - 1]
-}
 
-// fn main() {
-// 	let source = "alignment";
-// 	let target = "assignment";
-// 	println!(
-// 		"It's necessary to make {} change(s) to {}, to get {}",
-// 		edit_distance(source, target),
-// 		source,
-// 		target
-// 	);
-// }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_distance() {
-        assert_eq!(edit_distance("gumbo", "gambol"), 2);
-        assert_eq!(edit_distance("kitten", "sitting"), 3);
-        assert_eq!(edit_distance("rosettacode", "raisethysword"), 8);
-    }
+    v0[n]
 }
