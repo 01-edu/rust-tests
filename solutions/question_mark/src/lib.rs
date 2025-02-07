f9f5a0300a99c62cfb2cc4c14ac221ef61f9b08c
Pedro Ferreira <pedro.ferreira@01talent.com>
2025-02-07 16:01:16 +0000

    fix(question_mark)

--- solutions/question_mark/src/lib.rs
+++ solutions/question_mark/src/lib.rs
@@ -1,45 +1,3 @@
-/*
-## question_mark
-
-### Instructions
-
-You will have to create 3 structures:
-
-- `One`, that contains one element called `first_layer` it should be an `Option` for the structure `Two`.
-- `Two`, that contains one element called `second_layer` it should be an `Option` for the structure `Three`.
-- `Three`, that contains one element called `third_layer` it should be an `Option` for the structure `Four`.
-- `Four`, that contains one element called `fourth_layer` it should be an `u16` that is an `Option`.
-
-Beside the structure you must create a function named `get_fourth_layer` that is associated to the `One` structure.
-This function should return the `Option` value in the `Four` structure.
-
-### Example
-
-```rust
-fn main() {
-    let a = One {
-        first_layer : Some(Two {
-            second_layer: Some(Three {
-                third_layer: Some(Four {
-                    fourth_layer: Some(1000)
-                })
-            })
-        })
-    };
-
-    // output: 1000
-    println!("{:?}", match a.get_fourth_layer() {
-        Some(e) => e,
-        None => 0
-    })
-}
-```
-
-### Notions
-
-- https://doc.rust-lang.org/stable/rust-by-example/error/option_unwrap/question_mark.html
-
-*/
 #[derive(Clone, Copy)]
 pub struct One {
     pub first_layer: Option<Two>,
@@ -61,35 +19,7 @@ pub struct Four {
 }
 
 impl One {
-    pub fn get_fourth_layer(&self) -> Option<u16> {
+    pub fn get_fourth_layer(self) -> Option<u16> {
         self.first_layer?.second_layer?.third_layer?.fourth_layer
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_value() {
-        let a = One {
-            first_layer: Some(Two {
-                second_layer: Some(Three {
-                    third_layer: Some(Four {
-                        fourth_layer: Some(1000),
-                    }),
-                }),
-            }),
-        };
-        let b = One {
-            first_layer: Some(Two {
-                second_layer: Some(Three {
-                    third_layer: Some(Four {
-                        fourth_layer: Some(3),
-                    }),
-                }),
-            }),
-        };
-        assert_eq!(a.get_fourth_layer(), Some(1000));
-        assert_eq!(b.get_fourth_layer(), Some(3));
     }
 }
