b4f3a277479705ab522b1bcb8f3ffda8504e50c8
Pedro Ferreira <pedro.ferreira@01talent.com>
2025-02-19 14:14:59 +0000

    fix(error_types)

--- solutions/error_types/src/lib.rs
+++ solutions/error_types/src/lib.rs
@@ -2,14 +2,14 @@ pub use chrono::{NaiveDate, Utc};
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    pub form_values: (String, String),
+    pub form_values: (&'static str, String),
     pub date: String,
-    pub err: String,
+    pub err: &'static str,
 }
 
 impl FormError {
-    pub fn new(field_name: String, field_value: String, err: String) -> FormError {
-        FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        Self {
             form_values: (field_name, field_value),
             date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
             err,
@@ -19,73 +19,34 @@ impl FormError {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Form {
-    pub first_name: String,
-    pub last_name: String,
-    pub birth: NaiveDate,
-    pub birth_location: String,
+    pub name: String,
     pub password: String,
 }
 
 impl Form {
-    pub fn new(
-        first_name: String,
-        last_name: String,
-        birth: NaiveDate,
-        birth_location: String,
-        password: String,
-    ) -> Form {
-        Form {
-            first_name,
-            last_name,
-            birth,
-            birth_location,
-            password,
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new(
+                "first_name",
+                self.name.clone(),
+                "Username is empty",
+            ));
         }
-    }
 
-    pub fn validate(&self) -> Result<Vec<&str>, FormError> {
-        let v: Vec<&str> = vec![
-            match &self.first_name {
-                x if x == &String::from("") => Err(self.user_name()),
-                _ => Ok("Valid first name"),
-            }?,
-            match &self.password {
-                x if x.len() < 8 => Err(self.has_eight_char()),
-                _ if !self.validate_ascii_char() => Err(self.has_diff_ascii_char()),
-                _ => Ok("Valid password"),
-            }?,
-        ];
-        Ok(v)
-    }
+        let password_error =
+            |m: &'static str| Err(FormError::new("password", self.password.clone(), m));
 
-    fn user_name(&self) -> FormError {
-        FormError::new(
-            String::from("first_name"),
-            self.first_name.clone(),
-            String::from("No user name"),
-        )
-    }
+        if self.password.len() < 8 {
+            return password_error("Password should be at least 8 characters long");
+        } else if !(self.password.chars().any(|c| c.is_ascii_digit())
+            && self.password.chars().any(|c| c.is_ascii_alphabetic())
+            && self.password.chars().any(|c| c.is_ascii_punctuation()))
+        {
+            return password_error(
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            );
+        }
 
-    // all this is the part to validate all the parts of the form
-    fn has_eight_char(&self) -> FormError {
-        FormError::new(
-            String::from("password"),
-            self.password.clone(),
-            String::from("At least 8 characters"),
-        )
+        Ok(())
     }
-
-    fn has_diff_ascii_char(&self) -> FormError {
-        FormError::new(String::from("password"), self.password.clone(), String::from("Combination of different ASCII character types (numbers, letters and none alphanumeric characters)"))
-    }
-
-    fn validate_ascii_char(&self) -> bool {
-        let chars: Vec<char> = self.password.chars().collect();
-        chars.iter().any(|c| c.is_digit(10)) && chars.iter().any(|c| !c.is_alphanumeric())
-    }
-}
-
-#[allow(dead_code)]
-fn create_date(date: &str) -> NaiveDate {
-    NaiveDate::parse_from_str(date, "%Y-%m-%d").unwrap()
 }
