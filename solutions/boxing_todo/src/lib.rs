286b8864665b8bf117167608aab84655f1aba09c
Pedro Ferreira <me.pedro.ferreira@gmail.com>
2025-03-14 19:53:10 +0000

    fix(rust-quest03): applied proposed changes

--- solutions/boxing_todo/src/lib.rs
+++ solutions/boxing_todo/src/lib.rs
@@ -1,9 +1,8 @@
 mod err;
 pub use err::{ParseErr, ReadErr};
-pub use std::error::Error;
 
-use json::{parse, stringify};
-use std::fs::read_to_string;
+use std::error::Error;
+use std::fs;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Task {
@@ -12,55 +11,33 @@ pub struct Task {
     pub level: u32,
 }
 
-impl Task {
-    pub fn new(id: u32, description: String, level: u32) -> Task {
-        Task {
-            id,
-            description,
-            level,
-        }
-    }
-}
-
 #[derive(Debug, Eq, PartialEq)]
 pub struct TodoList {
     pub title: String,
     pub tasks: Vec<Task>,
 }
+
 impl TodoList {
-    pub fn new(title: String, tasks: Vec<Task>) -> TodoList {
-        TodoList { title, tasks }
-    }
     pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
-        let todo_raw = read_todos(path);
-        let parsed_todos = parse_todos(&todo_raw?)?;
-        Ok(parsed_todos)
-    }
-}
+        let contents = fs::read_to_string(path).map_err(|e| ReadErr {
+            child_err: Box::new(e),
+        })?;
 
-pub fn read_todos(path: &str) -> Result<String, Box<dyn Error>> {
-    let raw = read_to_string(path).map_err(|e| ReadErr {
-        child_err: Box::new(e),
-    })?;
-    Ok(raw)
-}
+        let contents = json::parse(&contents).map_err(|e| ParseErr::Malformed(Box::new(e)))?;
+        if contents["tasks"].is_empty() {
+            return Err(ParseErr::Empty.into());
+        }
 
-pub fn parse_todos(todo_str: &str) -> Result<TodoList, Box<dyn Error>> {
-    let parset = parse(todo_str).map_err(|e| ParseErr::Malformed(Box::new(e)))?;
-    if parset["tasks"].is_empty() {
-        return Err(ParseErr::Empty.into());
+        Ok(Self {
+            title: contents["title"].as_str().unwrap().to_owned(),
+            tasks: contents["tasks"]
+                .members()
+                .map(|m| Task {
+                    id: m["id"].as_u32().unwrap(),
+                    description: m["description"].as_str().unwrap().to_owned(),
+                    level: m["level"].as_u32().unwrap(),
+                })
+                .collect(),
+        })
     }
-    let mut v = vec![];
-    for i in 0..parset["tasks"].len() {
-        let a = &parset["tasks"][i];
-        let task = stringify(a["description"].clone());
-        v.push(Task::new(
-            stringify(a["id"].clone()).parse().unwrap(),
-            task.get(1..task.len() - 1).unwrap().to_string(),
-            stringify(a["level"].clone()).parse().unwrap(),
-        ));
-    }
-    let title = stringify(parset["title"].clone());
-    let todo_list = TodoList::new(title.get(1..title.len() - 1).unwrap().to_string(), v);
-    Ok(todo_list)
 }
--- solutions/panic/src/lib.rs
+++ solutions/panic/src/lib.rs
@@ -1,40 +1,5 @@
-/*
-## error types
-
-### Instructions
-
-Write a function that tries to open a file and panics if the file
-doesn't exist
-
-*/
-
 use std::fs::File;
 
 pub fn open_file(s: &str) -> File {
-    let file = File::open(s);
-    if let Ok(f) = file {
-        return f;
-    } else {
-        panic!("File not found")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs;
-
-    #[test]
-    #[should_panic(expected = "File not found")]
-    fn test_opening() {
-        open_file("file.txt");
-    }
-
-    #[test]
-    fn test_opening_existing() {
-        let filename = "created.txt";
-        File::create(filename).unwrap();
-        open_file(filename);
-        fs::remove_file(filename).unwrap();
-    }
+    File::open(s).unwrap()
 }
