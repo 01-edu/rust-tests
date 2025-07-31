a38b2f91f5e3e38920de07d5c6bd8d6cec20623e
Abdelilah SADIQUI <143453064+sadiqui@users.noreply.github.com>
2025-07-31 14:00:08 +0100

    refine(reverse_it): faster and more readable solution (#162)
    * refactor(solutions/reverse_it): faster and more readable solution
    
    - Eliminates unnecessary borrowing (&nbr)
    - Avoids intermediate collection (Vec<char>)
    
    * refactor(solutions/reverse_it): more concise and idiomatic code

--- solutions/reverse_it/src/lib.rs
+++ solutions/reverse_it/src/lib.rs
@@ -1,11 +1,8 @@
 pub fn reverse_it(nbr: i32) -> String {
-    let s = &nbr.to_string();
-    let str_nbr = s.trim_start_matches('-');
-    let rev: Vec<char> = str_nbr.chars().rev().collect();
-    if nbr >= 0 {
-        return format!("{}{}", rev.into_iter().collect::<String>(), str_nbr);
-    }
-    return format!("-{}{}", rev.into_iter().collect::<String>(), str_nbr);
+    let s = nbr.to_string();
+    let abs_s = s.trim_start_matches('-');
+    let rev = abs_s.chars().rev().collect::<String>();
+    format!("{}{}{}", if nbr < 0 { "-" } else { "" }, rev, abs_s)
 }
 
 #[cfg(test)]
